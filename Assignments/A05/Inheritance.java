7a59474a57017cc662fc2eb2ae32933d0605168d
Loic <konanloic10@gmail.com>
2021-09-13 14:13:19 -0500

    README.md

---
+++ Assignments/A05/Inheritance.java
@@ -0,0 +1,15 @@
+/**
+ * This is a program about Inheritance
+ * @version SE9
+ * @author Loic Konan
+ */
+
+class Inheritance
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+
+
+}

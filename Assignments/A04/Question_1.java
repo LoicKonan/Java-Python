d4c9995bf4e3fbbbc2e780f6566c6324d4652c94
Loic <konanloic10@gmail.com>
2021-09-15 09:45:25 -0500

    README.md

--- Assignments/A04/Question_1.java
+++ Assignments/A04/Question_1.java
@@ -8,7 +8,7 @@
  * 
  * Description:
  *              This Project is about taking an array with 20 elements, 
- *              and finding the largest and smallest number in that array.
+ *              and finding the large and small number in that array.
  *
  * Files:   
  *              Question_1.java
@@ -17,36 +17,47 @@
 
 public class Question_1 
 {
+    // The Driver program.
     public static void main(String[] args) 
     {
         // give array of 20 values
-        int [] NumArr = new int[] { 
+        int [] myArray = new int[] { 
                                     0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 
                                     10, 10, 12, 13, 14, 15, 16, 17, 18, 19 
                                    };
         
-        int Smallest = NumArr[0];
-        int Largest = NumArr[0];
+        int small = myArray[0];                   // smallest element as the first index.  
+        int large = myArray[0];                  // largeest element as the first index. 
        
-        for (int i = 1; i < NumArr.length; i++) 
+
+        /**
+         * In this for loop we will determine the smallest and the largest element in our
+         * array. We will compare each element to the first element, if we find the largest and the smallest, 
+         * we will then assign the largest one to the variable large and the smallest to the variable small. 
+         */
+        for (int i = 1; i < myArray.length; i++) 
         {
-            if (NumArr[i] > Largest)
-                {
-                    Largest = NumArr[i];
+            if (myArray[i] > large)             // Using this if statement to find the 
+                {                               // Largest element then assign it to the variable large.
+                    large = myArray[i];
                 }
 
-            else if (NumArr[i] < Smallest)
-               {
-                    Smallest = NumArr[i];
+            else if (myArray[i] < small)        // Using this else if statement to find the 
+               {                                // smallest element then assign it to the variable small.
+                    small = myArray[i];
                }
-        }
+        }                                       // End of for loop
 
+        /**
+         * Printing out our array using a for loop, then
+         * displaying the large and the small number from the array.
+         */
         System.out.println("These are the 20 elements of this Array: ");
-        for (int i = 0; i < NumArr.length ; i++) 
+        for (int i = 0; i < myArray.length ; i++)           
         {
-            System.out.print(NumArr[i] + " ");
+            System.out.print(myArray[i] + " ");
         }        
-        System.out.println("\n\nThe Smallest number of this array is: " + Smallest);
-        System.out.println("The Largest number of this array is: " + Largest + "\n");
-    }
+        System.out.println("\n\nThe small number of this array is: " + small);
+        System.out.println("The large number of this array is: " + large + "\n");
+    }                                            // End of the drive program.
 }

ec4cf97bb2129a38a639b078c66ff1207f47067b
Loic <konanloic10@gmail.com>
2021-09-22 14:49:11 -0500

    README.md

--- Assignments/A07/BankDemo.java
+++ Assignments/A07/BankDemo.java
@@ -1,9 +1,37 @@
+/**
+ * This is program about CheckingAccount class.
+ * 
+ * @version SE9
+ * @author Loic Konan
+ * Semester: Fall 2021
+ * Date: 9/22/2021
+ * 
+ */
+
 public class BankDemo 
 {
     public static void main(String[] args)
     {
-        CheckingAccount objChecking = new CheckingAccount(40005);
-        
+        CheckingAccount obj1 = new CheckingAccount(40005);
+        System.out.println("Depositing...........");
+        obj1.deposit(100000);
+        try
+        {
+            System.out.println("Withdrawing ...$100");
+            obj1.withdraw(100);
+
+
+            System.out.println("Withdrawing ........$1000000");
+            obj1.withdraw(1000000);
+        }
+        catch (Except e)
+        {
+            System.out.println("Sorry! You are shourt of " + e.getAmount());
+            e.printStackTrace();
+        }
+        finally
+        {
+            System.out.println("Come later");
+        }
     }
-    
 }

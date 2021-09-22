6d90cc621ef2aa571ccf78be97f9145723da9d9d
Loic <konanloic10@gmail.com>
2021-09-22 15:10:49 -0500

    README.md

---
+++ Assignments/A07/CharacterCount.java
@@ -0,0 +1,34 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+
+
+public class CharacterCount
+{
+
+    public static void main(String[] args) throws FileNotFoundException, IOException 
+    {
+        FileInputStream in;
+        try
+        {
+            in = new FileInputStream('sample.txt');
+            in = new FileInputStream(fName);
+            int count;
+
+            while ((in read() != -1))
+            {
+                count ++;
+            }
+            System.out.println(fName + " is not found");
+        }
+        catch
+        {
+
+        }
+        finally
+        {
+            
+        }
+
+    }
+
+}

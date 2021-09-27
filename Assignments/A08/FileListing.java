2ba40d5da824cac32e5c3eb3e3a2a90deb14ca4c
Loic <konanloic10@gmail.com>
2021-09-27 15:08:46 -0500

    README.md

---
+++ Assignments/A08/FileListing.java
@@ -0,0 +1,36 @@
+/**
+ * This is program about  class.
+ * 
+ * @version SE9
+ * @author Loic Konan
+ * Semester: Fall 2021
+ * Date: 9/27/2021
+ * 
+ */
+
+ import java.io.File;
+
+ class FileListing
+ {
+     public static void main(String[] args)
+     {
+         File file = null;
+         String [] paths;
+
+         try
+         {
+            file = new File("C:/Users/loick/Documents/");
+            paths = file.list();
+            for(String path:paths)
+            {
+                System.out.println(paths);
+                System.out.println("\n");
+
+            }
+         }
+         catch(Exception e)
+         {
+            e.printStackTrace();
+         }
+     }
+ }

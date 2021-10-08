e6134830db50d8f7fd5223db9136ab78d5d673cf
Loic <konanloic10@gmail.com>
2021-10-08 00:16:18 -0500

    README.md

--- Assignments/P02/Question_1.java
+++ Assignments/P02/Question_1.java
@@ -1,38 +1,30 @@
 /******************************************************************************
- * Author:      Loic Konan 
- * Email:       loickonan.lk@gmail.com 
- * Label:       Question 1
- * Title:       Programing Assignment 02
- * Course:      CMPS 4143 
- * Semester:    Fall 2021 
- * Date:        10/10/2021
+ * Author:          Loic Konan 
+ * Email:           loickonan.lk@gmail.com 
+ * Label:           Question 1 
+ * Title:           Programing Assignment 02 
+ * Course:          CMPS 4143 
+ * Semester:        Fall 2021 
+ * Date:            10/10/2021
  * 
- * Description:
- *              Designing an OOP code that have the following features:  
- *              inheritance(any), polymorphism (runtime and compile time), 
- *              abstraction and encapsulation.
+ * Description:     
+ *                  Designing an OOP code that have the following features:
+ *                  inheritance(any), polymorphism (runtime and compile time), 
+ *                  abstraction and encapsulation.
  * 
- * Solution:    
- *              
- *              
+ * Solution:
+ * 
+ * 
  *
- * Files:   
- *              Question_1.java
- *  
+ * Files:           Question_1.java
+ * 
  *****************************************************************************/
 
 
-
-
-
-
-
-
-
-
-
- 
 public class Question_1 
 {
-    
+    public static void main(String[] args) 
+    {
+        
+    }
 }

3dc152ec33fe959fb8fae0ebdb7c987a2f8d421c
Loic <konanloic10@gmail.com>
2021-10-07 22:39:07 -0500

    README.md

--- Assignments/P02/Question_2.java
+++ Assignments/P02/Question_2.java
@@ -1,3 +1,24 @@
+/******************************************************************************
+ * Author:      Loic Konan 
+ * Email:       loickonan.lk@gmail.com 
+ * Label:       Question 2
+ * Title:       Programing Assignment 02
+ * Course:      CMPS 4143 
+ * Semester:    Fall 2021 
+ * Date:        10/10/2021
+ * 
+ * Description:
+ *              
+ * 
+ * Solution:
+ *              
+ *
+ * Files:   
+ *              Question_1.java
+ *  * 
+ *****************************************************************************/
+
+
 public class Question_2 
 {
     
--- Assignments/P02/Question_3.java
+++ Assignments/P02/Question_3.java
@@ -1,3 +1,24 @@
+/******************************************************************************
+ * Author:      Loic Konan 
+ * Email:       loickonan.lk@gmail.com 
+ * Label:       Question 3
+ * Title:       Programing Assignment 02
+ * Course:      CMPS 4143 
+ * Semester:    Fall 2021 
+ * Date:        10/10/2021
+ * 
+ * Description:
+ *              
+ * 
+ * Solution:
+ *              
+ *
+ * Files:   
+ *              Question_1.java
+ *  * 
+ *****************************************************************************/
+
+
 public class Question_3 
 {
     

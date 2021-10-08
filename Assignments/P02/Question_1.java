ff43a65d924b4490098246ca94f88fe5e1c4a7c1
Loic <konanloic10@gmail.com>
2021-10-08 01:53:44 -0500

    README.md

--- Assignments/P02/Question_1.java
+++ Assignments/P02/Question_1.java
@@ -73,6 +73,30 @@ class Rectangle extends Shape
     }
 }  
 
+
+
+// Abstract class
+abstract class Animal 
+{
+    // Abstract method (does not have a body)
+    public abstract void animalSound();
+    // Regular method
+    public void sleep() 
+    {
+      System.out.println("Zzz");
+    }
+  }
+  
+  // Subclass (inherit from Animal)
+  class Pig extends Animal 
+  {
+    public void animalSound() 
+    {
+      // The body of animalSound() is provided here
+      System.out.println("The pig says: wee wee");
+    }
+  }
+
 /**
  * 
  */

0ba92c7b7136f303b52ab6ba6b5e2535e64a8c84
Loic <konanloic10@gmail.com>
2021-10-09 02:22:23 -0500

    README.md

--- Assignments/P02/Question_1.java
+++ Assignments/P02/Question_1.java
@@ -21,6 +21,33 @@
  *****************************************************************************/
 
 
+ 
+/**
+ * Abstract class to demonstrate Abstration. 
+ */
+abstract class Animal 
+{
+    // Abstract method (does not have a body)
+    public abstract void animalSound();
+
+    // Regular method
+    public void sleep() 
+    {
+      System.out.println("Zzz");
+    }
+  }
+  
+  // Subclass (inherit from Animal)
+  class Pig extends Animal 
+  {
+    public void animalSound() 
+    {
+      // The body of animalSound() is provided here
+      System.out.println("The pig says: wee wee");
+    }
+  }
+
+
 /**
  *  Parent class.
  */
@@ -31,6 +58,7 @@
         System.out.println("I'm the Parent class called: Country aka USA.");
     }  
 }  
+
 
 /**
  * Single Inheritance.

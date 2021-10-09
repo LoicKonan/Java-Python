6d56def875743cd85357a0a790678e76feab29d7
Loic <konanloic10@gmail.com>
2021-10-09 03:39:10 -0500

    README.md

--- Assignments/P02/Question_1.java
+++ Assignments/P02/Question_1.java
@@ -21,71 +21,69 @@
  *****************************************************************************/
 
 /**
- * Abstract class to demonstrate Abstration. 
+ * Abstract class to demonstrate Abstration.
  */
-  abstract class Continent  
-  {
-   // Abstract method (does not have a body)
-   public abstract void seasons();
+abstract class Continent 
+{
+    /**
+     * Abstract method (does not have a body)
+     */
+    public abstract void seasons();
 
-   // Regular method
-   public void weather() 
-   {
-     System.out.println("Beautiful weather on this Continent!!!");
-   }
-  }
-
+    // Regular method
+    public void weather() 
+    {
+        System.out.println("Beautiful weather on this Continent!!!");
+    }
+}
 
 /**
  * Subclass (inherit from Continent)
  */
- class Country extends Continent
-{  
+class Country extends Continent 
+{
     /**
-     * Runtime Polymorphism.
-     * This method display the information of the class.
+     * Runtime Polymorphism. This method display the information of the class.
      */
     void info() 
     {
         System.out.println("I'm a Subclass called: Country (inherit from Continent).");
-    } 
+    }
 
     public void seasons() 
     {
-      // The body of seasons() is provided here
-      System.out.println("There is 4 Seasons in this Country, well supposedly...");
-    } 
-}  
-
+        // The body of seasons() is provided here
+        System.out.println("There is 4 Seasons in this Country, well supposedly...");
+    }
+}
 
 /**
  * Subclass (inherit from Country)
  */
-class State extends Country
-{  
+class State extends Country 
+{
     private String capital;
 
     /**
      * Constructor.
      */
-    State(String capital)
+    State(String capital) 
     {
         this.capital = capital;
     }
 
-     /**
-     * Runtime Polymorphism.
-     * This method display the information of the class.
+    /**
+     * Runtime Polymorphism. This method display the information of the class.
      */
     @Override
-    void info()
+    void info() 
     {
         System.out.println("I'm a Subclass called: State (inherit from Country).");
     }
 
     /**
-     * Encapsulation.
-     * Mutator Function.
+     * Encapsulation. Mutator Function.
+     * 
      * @param capital
      */
     public void set_capital(String capital) 
@@ -94,36 +92,32 @@ class State extends Country
     }
 
     /**
-     * Encapsulation.
-     * Accessor Function.
+     * Encapsulation. Accessor Function.
+     * 
      * @return void
      */
     public String get_capital() 
     {
         return capital;
     }
-    
+
     /**
-     * Demonstration of Compile time Polymorphism.
-     * 1st method with the name population.
-     * This method to calculate the number of people
-     * in a State, by adding the number of males and females.
+     * Demonstration of Compile time Polymorphism. 1st method with the name
+     * population. This method to calculate the number of people in a State, by
+     * adding the number of males and females.
      * 
      * @param males
      * @param females
-     * @return int 
+     * @return int
      */
-    public int population(int males, int females)
-    { 
+    public int population(int males, int females) {
         return males + females;
     }
-    
+
     /**
-     * Demonstration of Compile time Polymorphism.
-     * 2nd method with the name population.
-     * This method calculate the number of 
-     * students in a University, by adding the 
-     * number of sophomore, freshmen, juniors and seniors.
+     * Demonstration of Compile time Polymorphism. 2nd method with the name
+     * population. This method calculate the number of students in a University, by
+     * adding the number of sophomore, freshmen, juniors and seniors.
      * 
      * @param sophomore
      * @param freshmen
@@ -131,22 +125,18 @@ class State extends Country
      * @param seniors
      * @return int
      */
-    public int population(int sophomore, int freshmen, int juniors, int seniors)
-    {
+    public int population(int sophomore, int freshmen, int juniors, int seniors) {
         return sophomore + freshmen + juniors + seniors;
     }
-}  
+}
 
-
-public class Question_1 
-{
-    public static void main(String[] args) 
-    {
-        State s = new State("Texas");  
+public class Question_1 {
+    public static void main(String[] args) {
+        State s = new State("Texas");
 
         System.out.println("Capital is " + s.get_capital());
 
         s.info();
-        s.info();   
+        s.info();
     }
 }

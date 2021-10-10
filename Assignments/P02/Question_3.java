ad1c4857cc877c12780ccf3b2a824e5ebab1da3f
Loic <konanloic10@gmail.com>
2021-10-10 14:45:53 -0500

    README.md

--- Assignments/P02/Question_3.java
+++ Assignments/P02/Question_3.java
@@ -48,10 +48,62 @@ public class calculator
     }
 }
 
+class SyntaxError extends Exception 
+        {
+                private String ErrorMessage;
+
+                // when running through the exceptions, we use pointer type to error to point 
+                // to the current TestingCase and ErrorMessage
+                public SyntaxError(String ErrorMessage) 
+                {
+                        //point as this specific error message from the expression
+                        this.ErrorMessage = ErrorMessage;
+                }
+
+                //create an overrriding function that grabes the stnatax error and the
+                //corresponding error associated with it
+                @Override
+                public String getLocalizedMessage() 
+                {
+                        //returning the throw exception user define for the type of error
+                        return this.ErrorMessage;
+                }
+        }
+
+        // created and extended class runtime error that extends the exception
+        class RuntimeError extends Exception 
+        {
+                private String ErrorMessage;
+
+                public RuntimeError(String ErrorMessage) 
+                {
+                        //pointer to type to point to specific instance of the exception 
+                        //hadnler
+                        this.ErrorMessage = ErrorMessage;
+                }
+
+                //we override the ErrorMessage to print out to the screen the ErrorMessage that is 
+                // capture on compilation
+                @Override
+                public String getLocalizedMessage() 
+                {
+                        //returning the error message capture from the throws of the 
+                        //individualized errors
+                 return  this.ErrorMessage+"\n";
+                }
+        }
+
 
 public class Question_3 
 {
     public static void main(String[] args) 
     {
+        File file = new File("input.txt");
+        Scanner scanner = new Scanner(file);
+
+        while (scanner.hasNextLine())
+        {
+            System.out.println(scanner.nextLine());
+        }
     }
 }

d3cdc3b0c06c0cda8d12549602212c91c2836d44
Loic <konanloic10@gmail.com>
2021-10-11 14:47:55 -0500

    README.md

--- Assignments/A09/Main.py
+++ Assignments/A09/Main.py
@@ -1,3 +1,26 @@
-print(12)
+# 3 types of vars, int float, str;
 
-print('Hello world')
+var = '3'
+
+print(type(var))
+var = int(var)
+print(type(var))
+
+var1 = '3'
+var2 = var * 3
+print(type(var2))
+
+arr = [0] 
+arr = arr * 3
+arr = [0,0,0]
+print(arr)
+
+
+var = 3
+
+if var < 10:
+    print('var is less than 10')
+else:
+    print('var is greater')
+    
+print('End')

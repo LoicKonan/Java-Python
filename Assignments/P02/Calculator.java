e50f1b3016108923f4a76ecd506d01a1fba7b861
Loic <konanloic10@gmail.com>
2021-10-10 19:27:14 -0500

    README.md

--- Assignments/P02/Calculator.java
+++ Assignments/P02/Calculator.java
@@ -91,29 +91,7 @@ public class Calculator
 
     public static void main(String[] args)  throws SyntaxError, RuntimeError 
     {
-        // create random object
-        Random ran = new Random();
-  
-        // generating integer
-        int nxt = ran.nextInt();
-    
-        // Printing the random Number.
-        System.out.println
-        ("The Randomly generated integer is : " + nxt);
-
-        int day = 4;
-        
-        switch (day) 
-        {
-            case 6:
-            System.out.println("Today is Saturday");
-            break;
-            case 7:
-            System.out.println("Today is Sunday");
-            break;
-            default:
-            System.out.println("Looking forward to the Weekend");
-        }
+        System.out.println("The Randomly generated integer is : ");
 
         Scanner input = new Scanner(System.in);
 

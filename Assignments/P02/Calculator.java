a2040968d7ba14767ec1914308024416010f2d08
Loic <konanloic10@gmail.com>
2021-10-11 21:23:41 -0500

    README.md

--- Assignments/P02/Calculator.java
+++ Assignments/P02/Calculator.java
@@ -22,189 +22,16 @@
 //  * 
 //  *****************************************************************************/
 
-// import java.util.*;
-
-// class SyntaxError extends Exception 
-// {
-//     private String ErrorMessage;
-
-//     public SyntaxError(String ErrorMessage) 
-//     {
-//         this.ErrorMessage = ErrorMessage;
-//     }
-
-//     @Override
-//     public String getLocalizedMessage() 
-//     {
-//         return this.ErrorMessage;
-//     }
-// }
-
-
-// class RuntimeError extends Exception 
-// {
-//     private String ErrorMessage;
-
-//     public RuntimeError(String ErrorMessage) 
-//     {
-//         this.ErrorMessage = ErrorMessage;
-//     }
-
-//     @Override
-//     public String getLocalizedMessage() 
-//     {
-//         return  this.ErrorMessage+"\n";
-//     }
-// }
-
-// static void checkString(String input_user) throws SyntaxError, RuntimeError 
-// {
-//     boolean equalSign = false;                                              // Number of equal sign.
-//     int pareLeft = 0;                                                       // Number of '(' symbols.
-//     int pareRight = 0;                                                      // Number of ')' symbols.
-//     int addCount = 0;                                                       // Number of '+' symbols.
-//     int subCount = 0;                                                       // Number of '-' symbols.
-//     int mulCount = 0;                                                       // Number of '*' symbols.
-//     int divCount = 0;                                                       // Number of '/' symbols.
-//     int modCount = 0;                                                       // Number of '%' symbols.
-
-//                                                                             // Throw a Syntax error if an equal sign is not in the correct position
-//     if (input_user.charAt(1) == '=') 
-//     {
-//         equalSign = true;
-        
-        
-//         /**
-//          * Loop through the string and check for the amount of operations needed
-//          */
-//         for (int i = 0; i < input_user.length(); i++) 
-//         {
-//             System.out.println(input_user.charAt(i));
-
-//             if (input_user.charAt(i) == '+') 
-//             {
-//                 addCount++;
-//                 symbolCheck(input_user, i);
-//             } 
-//             else if (input_user.charAt(i) == '-') 
-//             {
-//                 subCount++;
-//                 symbolCheck(input_user, i);
-//             } 
-//             else if (input_user.charAt(i) == '*') 
-//             {
-//                 mulCount++;
-//                 symbolCheck(input_user, i);
-//             } 
-//             else if (input_user.charAt(i) == '/') 
-//             {
-//                 divCount++;
-//                 symbolCheck(input_user, i);
-               
-//                 if (input_user.charAt(i + 1) == '0')                              // if Divide by 0 throw a Runtime error.
-//                 {
-//                     throw new RuntimeError(input_user);
-//                 }
-//             } 
-//             else if (input_user.charAt(i) == '%') 
-//             {
-//                 modCount++;
-//                 symbolCheck(input_user, i);
-                    
-//                 if (input_user.charAt(i + 1) == '0')                              // if Mod by 0 throw a Runtime Error.
-//                 {
-//                     throw new RuntimeError(input_user);
-//                 }
-//             } 
-//             else if (input_user.charAt(i) == '(') 
-//             {
-//                 pareLeft++;
-//             } 
-//             else if (input_user.charAt(i) == ')') 
-//             {
-//                 pareRight++;
-//             } 
-//             else if (input_user.charAt(i) == '=' && i != 1) 
-//             {
-//                 equalSign = false;
-//                 throw new SyntaxError(input_user);
-//             }
-//         }
-        
-//         /**
-//          * If the amount of parentheses doesn't match  
-//          * Throw an error for more ')' than '('.
-//          * else Throw an error for more '(' than ')'
-//          */
-//         if (pareLeft < pareRight) 
-//         {
-//             throw new SyntaxError(input_user);
-//         } 
-//         else if (pareRight < pareLeft) 
-//         {
-//             throw new SyntaxError(input_user);
-//         }
-//     } 
-//     else 
-//     {
-//         throw new SyntaxError(input_user);
-//     }
-// }
-
-//  static void symbolCheck(String input_user, int i) 
-// {
-    
-// }
-
-
-// public static class Calculator
-// {
-//     public static long Add(long a, long b)
-//     {
-//         return a + b;
-//     }
-    
-//     public long Subtraction(long a, long b)
-//     {
-//         return a - b;
-//     }
-
-//     public long Multiplication(long a, long b) 
-//     {
-//         return a * b;
-//     }
-
-//     public long Divide(long a, long b) 
-//     {
-//         return a / b;
-//     }
-    
-//     public long Modulo(long a, long b) 
-//     {
-//         return a % b;
-//     }
-
-//     public static void main(String[] args) 
-//     {
-//         System.out.println("Enter your Equation: ");
-        
-       
-//         Scanner input = new Scanner(System.in);
-//         String Equation = input.nextLine();
-
-        
-
-        
-//     }
-// }
-
+import java.io.File;                                       // Import the File class
+import java.io.FileNotFoundException;
+import java.util.Scanner;                                  // Import the Scanner class to read text files
 
 
 public class Calculator
 {
         class SyntaxError extends Exception 
         {
-                private final String ErrorMessage;
+                private String ErrorMessage;
 
                 public SyntaxError(String ErrorMessage) 
                 {
@@ -220,7 +47,7 @@ public class Calculator
 
         class RuntimeError extends Exception 
         {
-                private final String ErrorMessage;
+                private String ErrorMessage;
 
                 public RuntimeError(String ErrorMessage) 
                 {
@@ -234,15 +61,8 @@ public class Calculator
                 }
         }
 
-        public int TestCaseSolution(String equation) 
-        {
-                int result;
-                result=7*6/2;
-                System.out.println(equation + "                 " + result);
-                return 0;
-        }
+      
 
-        // use this method to check if there is any Syntax/runtime error
         public int getSolution(String TestingCase) throws RuntimeError, SyntaxError 
         {
                 int Solution = 0;
@@ -301,14 +121,14 @@ public class Calculator
                         throw new SyntaxError(TestingCase + "               Syntax Error: Unexpected '='\n");
                 }
 
-                Solution = TestCaseSolution(TestingCase);
+                Solution = getSolution(TestingCase);
 
                 return Solution;
                
                 
         }
 
-        public static void main(String[] args) throws Calculator.SyntaxError 
+        public static void main(String[] args) throws RuntimeException, FileNotFoundException, RuntimeError, SyntaxError         
         {
 
                 System.out.println("=======================================================|\n"+
@@ -317,83 +137,37 @@ public class Calculator
                                    "|   Test Expression           Correct Response         |\n"+
                                    "|======================================================|\n");
 
-                Calculator CalcTestCase = new Calculator();
 
+                Calculator CalcTestCase = new Calculator();          
                 
-                String TestCase = "X = 1+2+3"; 
-                try 
-                {
-                        CalcTestCase.getSolution(TestCase);
-                } 
                 
-                catch (RuntimeError e) 
+                File myObj = new File("calculator.txt");
+                Scanner myReader = new Scanner(myObj);
+                while (myReader.hasNextLine()) 
                 {
-                        System.err.println(e.getLocalizedMessage());
-                } 
+                        String data = myReader.nextLine();
+                        System.out.println(data);
 
-            
-                TestCase = "Y = 2+5 = 3";
-                try 
-                {
-                        CalcTestCase.getSolution(TestCase);
-                } 
-                catch (RuntimeError e) 
-                {
-                        System.err.println(e.getLocalizedMessage());
-                } 
-
-
-                TestCase = "Y = 6*Z+5";
-                try 
-                {
-                        CalcTestCase.getSolution(TestCase);
-                } 
-                catch (RuntimeError e) 
-                {
-                        System.err.println(e.getLocalizedMessage());
-                } 
-        
+                        try 
+                        {
+                                // go to getsolution block and see if anything fits the bill
+                                CalcTestCase.getSolution(data);
+                        } 
                 
+                        catch (RuntimeError e) 
+                        {
+                                //catch the runtime and syntax errors 
+                                System.err.println(e.getLocalizedMessage());
+                        } 
+                        catch (SyntaxError e) //catching the syntax errors
+                        {
+                                System.err.println(e.getLocalizedMessage());
+                        }
 
-                TestCase = "Y=3+5+(1+6)";
-                try 
-                {
-                    CalcTestCase.getSolution(TestCase);
-                } 
-                catch (RuntimeError e) 
-                {
-                        System.err.println(e.getLocalizedMessage());
-                } 
-               
-               
-                TestCase = "Z=(3+5)/0";
-                try 
-                {
-                    CalcTestCase.getSolution(TestCase);
                 }
+                myReader.close();
 
-                catch (RuntimeError e) 
-                {
-                        System.err.println(e.getLocalizedMessage());
-                }
-               
                 System.out.println("--------------------------------------------------------\n");
-
-                
-                TestCase = "A = 7*6/2";
-                try 
-                {
-                    CalcTestCase.getSolution(TestCase);
-                }
-
-                catch (RuntimeError e) 
-                {
-                        System.err.println(e.getLocalizedMessage());
-                }
-               
-                finally
-                {
-                        System.out.println("--------------------------------------------------------\n");
-                }   
+            
         }
 }

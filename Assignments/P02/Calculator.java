f957407a16ff04519b7d7ffd6d5086abd158d989
Loic <konanloic10@gmail.com>
2021-10-11 21:30:42 -0500

    README.md

--- Assignments/P02/Calculator.java
+++ Assignments/P02/Calculator.java
@@ -1,101 +1,120 @@
 
-// /******************************************************************************
-//  * Author:          Loic Konan 
-//  * Email:           loickonan.lk@gmail.com 
-//  * Label:           Question 3 
-//  * Title:           Programing Assignment 02 
-//  * Course:          CMPS 4143 
-//  * Semester:        Fall 2021 
-//  * Date:            10/10/2021
-//  * 
-//  * Description: 
-//  *                  Creating a calculator class to perform these operations:
-//  *                  Addition, Subtraction, Multiplication, Division, and Modulo. 
-//  *                  With these two exception handling classes: 
-//  *                  SyntaxError and RuntimeError.
-//  * 
-//  * Solution:
-//  *
-//  * 
-//  * 
-//  * Files:           Question_3.java
-//  * 
-//  *****************************************************************************/
+/******************************************************************************
+ * Author:          Loic Konan 
+ * Email:           loickonan.lk@gmail.com 
+ * Label:           Question 3 
+ * Title:           Programing Assignment 02 
+ * Course:          CMPS 4143 
+ * Semester:        Fall 2021 
+ * Date:            10/10/2021
+ * 
+ * Description: 
+ *                  Creating a calculator class to perform these operations:
+ *                  Addition, Subtraction, Multiplication, Division, and Modulo. 
+ *                  With these two exception handling classes: 
+ *                  SyntaxError and RuntimeError.
+ * 
+ * Solution:
+ *
+ * 
+ * 
+ * Files:           Question_3.java
+ * 
+ *****************************************************************************/
 
-import java.io.File; // Import the File class
+import java.io.File;                                    // Import the File class
 import java.io.FileNotFoundException;
-import java.util.Scanner; // Import the Scanner class to read text files
+import java.util.Scanner;                               // Import the Scanner class to read text files
 
 public class Calculator {
-        class SyntaxError extends Exception {
+        class SyntaxError extends Exception 
+        {
                 private String ErrorMessage;
 
-                public SyntaxError(String ErrorMessage) {
+                public SyntaxError(String ErrorMessage) 
+                {
                         this.ErrorMessage = ErrorMessage;
                 }
 
                 @Override
-                public String getLocalizedMessage() {
+                public String getLocalizedMessage() 
+                {
                         return this.ErrorMessage;
                 }
         }
 
-        class RuntimeError extends Exception {
+        class RuntimeError extends Exception 
+        {
                 private String ErrorMessage;
 
-                public RuntimeError(String ErrorMessage) {
+                public RuntimeError(String ErrorMessage) 
+                {
                         this.ErrorMessage = ErrorMessage;
                 }
 
                 @Override
-                public String getLocalizedMessage() {
+                public String getLocalizedMessage() 
+                {
                         return this.ErrorMessage + "\n";
                 }
         }
 
-        public int getSolution(String TestingCase) throws RuntimeError, SyntaxError {
+        public int getSolution(String TestingCase) throws RuntimeError, SyntaxError 
+        {
                 int Solution = 0;
                 int rightBrace = 0, leftBrace = 0, EqualsSigns = 0;
 
-                for (int i = 1; i < TestingCase.length(); i++) {
-                        if (TestingCase.charAt(i) == '(') {
+                for (int i = 1; i < TestingCase.length(); i++) 
+                {
+                        if (TestingCase.charAt(i) == '(') 
+                        {
                                 leftBrace++;
                         }
 
-                        if (TestingCase.charAt(i) == ')') {
+                        if (TestingCase.charAt(i) == ')') 
+                        {
                                 rightBrace++;
                         }
 
-                        if (TestingCase.charAt(i) == '=') {
+                        if (TestingCase.charAt(i) == '=') 
+                        {
                                 EqualsSigns++;
                         }
 
-                        if (TestingCase.charAt(i) == '/') {
-                                if (TestingCase.charAt(i + 1) == '0') {
+                        if (TestingCase.charAt(i) == '/') 
+                        {
+                                if (TestingCase.charAt(i + 1) == '0') 
+                                {
                                         throw new RuntimeError(TestingCase
                                                         + "                 Syntax Error: Divide by 0 occured");
                                 }
                         }
 
                         if ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                                        .contains("" + TestingCase.charAt(i))) {
+                                        .contains("" + TestingCase.charAt(i))) 
+                                        {
                                 throw new SyntaxError(
                                                 TestingCase + "                 Syntax Error: more than one variable");
                         }
 
                 }
 
-                if (leftBrace < rightBrace) {
+                if (leftBrace < rightBrace) 
+                {
                         throw new SyntaxError(TestingCase + "                Syntax Error: ')' expected. \n");
-                } else if (leftBrace > rightBrace) {
+                } 
+                else if (leftBrace > rightBrace) 
+                {
                         throw new SyntaxError(TestingCase + "                Syntax Error: '(' expected.");
                 }
 
-                if (EqualsSigns < 1) {
+                if (EqualsSigns < 1) 
+                {
                         throw new SyntaxError(TestingCase + "               Syntax Error: '=' expected  \n");
                 }
 
-                else if (EqualsSigns > 1) {
+                else if (EqualsSigns > 1) 
+                {
                         throw new SyntaxError(TestingCase + "               Syntax Error: Unexpected '='\n");
                 }
 
@@ -108,6 +127,12 @@ public class Calculator {
         public static void main(String[] args)
                         throws RuntimeException, FileNotFoundException, RuntimeError, SyntaxError {
 
+                System.out.println("=======================================================|\n"
+                                + "| Question 3                                           |\n"
+                                + "|                                                      |\n\n"
+                                + "|   Test Expression           Correct Response         |\n"
+                                + "|======================================================|\n");
+
                 Calculator CalcTestCase = new Calculator();
 
                 File myObj = new File("calculator.txt");
@@ -116,10 +141,22 @@ public class Calculator {
                         String data = myReader.nextLine();
                         System.out.println(data);
 
+                        try {
+                                // go to getsolution block and see if anything fits the bill
+                                CalcTestCase.getSolution(data);
+                        }
+
+                        catch (RuntimeError e) {
+                                // catch the runtime and syntax errors
+                                System.err.println(e.getLocalizedMessage());
+                        } catch (SyntaxError e) // catching the syntax errors
+                        {
+                                System.err.println(e.getLocalizedMessage());
+                        }
+
                 }
                 myReader.close();
 
                 System.out.println("--------------------------------------------------------\n");
-
         }
 }

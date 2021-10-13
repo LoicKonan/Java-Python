062012860c14a66c1b0d199089c7f48e7198e83f
Loic <konanloic10@gmail.com>
2021-10-13 12:45:13 -0500

    README.md

--- Assignments/P02/Question2/Question_2.java
+++ Assignments/P02/Question2/Question_2.java
@@ -1188,20 +1188,5 @@ public class Question_2
         Carnivora Carnivora4 = new Carnivora(4, true, true, 10, 60.0, 2300, "Meat", 0, null);
         Carnivora Carnivora5 = new Carnivora(4, true, true, 10, 60.0, 2300, "Meat", 0, null);
 
-    
-    
-        try (FileReader reader = new FileReader("Outfile.txt")) 
-        {
-            int character;
-         
-            while ((character = reader.read()) != -1) 
-            {
-                System.out.print((char) character);
-            }
-        } 
-        catch (IOException e) 
-        {
-            e.printStackTrace();
-        }
     }
 }

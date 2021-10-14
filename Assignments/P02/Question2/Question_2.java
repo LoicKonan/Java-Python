226c1e36927ba018e37bd1bcbb44dcb0c9b1043e
Loic <konanloic10@gmail.com>
2021-10-13 20:00:17 -0500

    README.md

--- Assignments/P02/Question2/Question_2.java
+++ Assignments/P02/Question2/Question_2.java
@@ -23,7 +23,6 @@
  *****************************************************************************/
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 

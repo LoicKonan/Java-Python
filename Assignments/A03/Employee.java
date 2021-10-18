96376e7021edceb256379c600e28c8f2ff649135
Loic <konanloic10@gmail.com>
2021-10-18 16:58:07 -0500

    README.md

--- Assignments/A03/Employee.java
+++ Assignments/A03/Employee.java
@@ -1,118 +1,88 @@
-// /**
-//  * This is an Employee class program in Java
-//  * 
-//  * @version SE9
-//  * @author Loic Konan Semester: Fall 2021 Date: 9/07/2021
-//  * 
-//  */
+/**
+ * This is an Employee class program in Java
+ * 
+ * @version SE9
+ * @author Loic Konan Semester: Fall 2021 Date: 9/07/2021
+ * 
+ */
 
-// public class Employee 
-// {
-//     String Name, designation, race;
-//     int age;
-//     double salary;
-
-//     public Employee(String Name, String designation, String race, int age, double salary) 
-//     {
-//         this.Name = Name;
-//         this.designation = designation;
-//         this.race = race;
-//         this.age = age;
-//         this.salary = salary;
-//     }
-
-//     public void setage(int age) 
-//     {
-//         this.age = age;
-//     }
-
-//     public int getage() 
-//     {
-//         return age;
-//     }
-
-//     public void setSalary(int salary) 
-//     {
-//         this.salary = salary;
-//     }
-
-//     public double getSalary() 
-//     {
-//         return salary;
-//     }
-
-//     public void setName(String Name) 
-//     {
-//         this.Name = Name;
-//     }
-
-//     public String getName() 
-//     {
-//         return Name;
-//     }
-
-//     public void setdesignation(String designation) 
-//     {
-//        this.designation = designation;
-//     }
-
-//     public String getdesignation() 
-//     {
-//         return designation;
-//     }
-
-//     public void setrace(String race) 
-//     {
-//         this.race = race;
-//     }
-
-//     public String getrace() 
-//     {
-//         return race;
-//     }
-
-//     public static void main(String args[]) 
-//     {
-
-//         Employee Employee1 = new Employee("TEK JAI", "Software Engineer", "African", 28, 500000);
-//         Employee Employee2 = new Employee("LEBRON JAMES", "BASKETBALL PLAYER ", "American", 38, 1000000);
-
-//         System.out.println(Employee1.getName() + "'s occupation is: " + Employee1.getdesignation()
-//                 + ", He is " + Employee2.getrace() + " and His Age is " + Employee1.getage() + " He get paid annually around: $" + Employee1.getSalary());
-
-//         System.out.println(Employee2.getName() + "'s occupation is  " + Employee2.getdesignation()
-//                 + ", He is " + Employee2.getrace() + " His Age is " + Employee2.getage() + " and He get paid annually around: $" + Employee2.getSalary());
-
-//     }
-// }
-
-abstract Dayclass
+public class Employee 
 {
+    String Name, designation, race;
+    int age;
+    double salary;
 
-}
-abstract monthclass
-{
-
-}
-
-class caclass extends Dayclass, monthclass
-{
-    public int d;
-    public int m;
-    public int y;
-    final int PI = 3.14;
-    public void print(int [][])
+    public Employee(String Name, String designation, String race, int age, double salary) 
     {
-        // return ()
+        this.Name = Name;
+        this.designation = designation;
+        this.race = race;
+        this.age = age;
+        this.salary = salary;
     }
-}
 
-public class Employee
-{
-    public static void main(String[] args)
+    public void setage(int age) 
     {
-        calclass N = new calclass();
-        sytem.out.println(N.print);
+        this.age = age;
+    }
+
+    public int getage() 
+    {
+        return age;
+    }
+
+    public void setSalary(int salary) 
+    {
+        this.salary = salary;
+    }
+
+    public double getSalary() 
+    {
+        return salary;
+    }
+
+    public void setName(String Name) 
+    {
+        this.Name = Name;
+    }
+
+    public String getName() 
+    {
+        return Name;
+    }
+
+    public void setdesignation(String designation) 
+    {
+       this.designation = designation;
+    }
+
+    public String getdesignation() 
+    {
+        return designation;
+    }
+
+    public void setrace(String race) 
+    {
+        this.race = race;
+    }
+
+    public String getrace() 
+    {
+        return race;
+    }
+
+    public static void main(String args[]) 
+    {
+
+        Employee Employee1 = new Employee("TEK JAI", "Software Engineer", "African", 28, 500000);
+        Employee Employee2 = new Employee("LEBRON JAMES", "BASKETBALL PLAYER ", "American", 38, 1000000);
+
+        System.out.println(Employee1.getName() + "'s occupation is: " + Employee1.getdesignation()
+                + ", He is " + Employee2.getrace() + " and His Age is " + Employee1.getage() + " He get paid annually around: $" + Employee1.getSalary());
+
+        System.out.println(Employee2.getName() + "'s occupation is  " + Employee2.getdesignation()
+                + ", He is " + Employee2.getrace() + " His Age is " + Employee2.getage() + " and He get paid annually around: $" + Employee2.getSalary());
+
     }
 }
 

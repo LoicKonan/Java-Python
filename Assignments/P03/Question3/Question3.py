6823c128ef0f178085eb9537c8b23afa04f2f6d4
Loic <konanloic10@gmail.com>
2021-10-19 01:52:32 -0500

    README.md

---
+++ Assignments/P03/Question3/Question3.py
@@ -0,0 +1,44 @@
+# A simple Python3 program for
+# implementation of atoi
+import sys
+ 
+def myAtoi(Str):
+ 
+    sign, base, i = 1, 0, 0
+     
+    # If whitespaces then ignore.
+    while (Str[i] == ' '):
+        i += 1
+     
+    # Sign of number
+    if (Str[i] == '-' or Str[i] == '+'):
+        sign = 1 - 2 * (Str[i] == '-')
+        i += 1
+ 
+    # Checking for valid input
+    while (i < len(Str) and
+          Str[i] >= '0' and Str[i] <= '9'):
+               
+        # Handling overflow test case
+        if (base > (sys.maxsize // 10) or
+           (base == (sys.maxsize // 10) and
+           (Str[i] - '0') > 7)):
+            if (sign == 1):
+                return sys.maxsize
+            else:
+                return -(sys.maxsize)
+         
+        base = 10 * base + (ord(Str[i]) - ord('0'))
+        i += 1
+     
+    return base * sign
+ 
+# Driver Code
+Str = list("-91283472332")
+ 
+# Functional Code
+val = myAtoi(Str)
+ 
+print(val)
+ 
+# This code is contributed by divyeshrabadiya07

4e8f8c84fcd79d34b058c4b03f51121a728aa5f7
Loic <konanloic10@gmail.com>
2021-10-20 14:36:31 -0500

    README.md

--- Assignments/A12/main.py
+++ Assignments/A12/main.py
@@ -13,3 +13,20 @@ res = addition(x,y)
     
 print(res)
 
+
+
+def fib(n):
+    if n < 1:
+        return -1
+    
+    elif n == 1 or n == 2:
+        return 1
+    
+    else:
+        return fib(n-1) + (n-2) 
+
+
+print("Give me nth fib number")
+x = int(input())
+fibn = fib(x)
+print(fibn)

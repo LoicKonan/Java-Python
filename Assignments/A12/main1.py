ad71d138290b91fef075c43e4316eb59e2a32c67
Loic <konanloic10@gmail.com>
2021-10-20 15:00:39 -0500

    README.md

--- Assignments/A12/main1.py
+++ Assignments/A12/main1.py
@@ -1,25 +1,24 @@
-# enter hours: 45
-# Enter rate: 10   
+# Python
+# @author Loic Konan
+# Semester: Fall 2021
+# Date: 10/20/2021 
+# 
 
-# pay: 475.0  
-
-#475 = 40 * 10 + 5 * 15
-
-
-def addition(param1,param2):
+def salary(param1,param2):
     
     if(param1 <= 40):
         return param1 * param2
+    
     elif (param1 > 40): 
         A = param1 - 40
         return 40 * param2 + A * 15 
 
 
 print('Enter your hours and your your rate: ')
-x = int(input())
-y = int(input()) 
+x = float(input())
+y = float(input()) 
 print('You have entered: ', x, y)
 
-res = addition(x,y)
+res = salary(x,y)
     
 print(res)

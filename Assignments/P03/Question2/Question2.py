6d8ba152f4dfa77a778fc1c6b6498be05299e31d
Loic <konanloic10@gmail.com>
2021-10-20 01:29:51 -0500

    README.md

--- Assignments/P03/Question2/Question2.py
+++ Assignments/P03/Question2/Question2.py
@@ -46,70 +46,73 @@
 # for Lname, Fname, age, job, addy in zip(Lname_array, Fname_array, age_array, Occupation_array, Address_array):
 #     print(Lname, '\t', Fname,'\t', age, '\t', job, '\t', addy)
 
+# print(FirstName, LastName, " aged", Age," years, worked as a ",Occupation,
+#              " and currently lives at ",Address,".\n")
  
-i=0
-entry=int(input("How many people would you like to add?"))
-while i<entry:
+ 
+# i=0
+# entry=int(input("How many people would you like to add?"))
+# while i<entry:
 
-    # now for the basis of the individuals
+#     # now for the basis of the individuals
     
-    while(True):
-        try:
-            FirstName=str(input("What is the persons first name?"))# user prompt
-        except:
-            raise ValueError('FirstName  is not valid')
-        if FirstName.isnumeric(): #check if the name is entered as a number
-            ErrorWarning= ValueError("Invalid input, insert correct First Name using non numeric values")
-            print(ErrorWarning)
-        else: 
-            break
+#     while(True):
+#         try:
+#             FirstName=str(input("What is the persons first name?"))# user prompt
+#         except:
+#             raise ValueError('FirstName  is not valid')
+#         if FirstName.isnumeric(): #check if the name is entered as a number
+#             ErrorWarning= ValueError("Invalid input, insert correct First Name using non numeric values")
+#             print(ErrorWarning)
+#         else: 
+#             break
         
-    while(True):
-        try:
-            LastName=str(input("What is the persons last  name?"))# user prompt
-        except:
-            raise ValueError('Last Name  is not valid')
-        if LastName.isnumeric(): #chek if occupation contains only numeric value or not
-            ErrorWarning= ValueError("Invalid input, insert correct Last Name using non numeric values")
-            print(ErrorWarning)
-        else: #else if the occupation is correct
-            break
+#     while(True):
+#         try:
+#             LastName=str(input("What is the persons last  name?"))# user prompt
+#         except:
+#             raise ValueError('Last Name  is not valid')
+#         if LastName.isnumeric(): #chek if occupation contains only numeric value or not
+#             ErrorWarning= ValueError("Invalid input, insert correct Last Name using non numeric values")
+#             print(ErrorWarning)
+#         else: #else if the occupation is correct
+#             break
     
 
-    # start try catch block that will catch invalid data  whcih consists of nums between 0 and 150
+#     # start try catch block that will catch invalid data  whcih consists of nums between 0 and 150
     
-    while True:
-        Age = input("Please Your age: ")
-        if Age.isdigit() and (0 < int(Age) < 150): 
-            break
-        else: print("Please Enter a valid age")
+#     while True:
+#         Age = input("Please Your age: ")
+#         if Age.isdigit() and (0 < int(Age) < 150): 
+#             break
+#         else: print("Please Enter a valid age")
                     
-    # try catch to read in valid non numeric occupation
-    while(True):
-        try:
-            Occupation= str(input("What is their occupation?"))# prompt the user
-        except:
-            raise ValueError('Occupation is not valid')# if invalid say invalid
-        if Occupation.isnumeric(): #chek if occupation contains only numeric value or not
-            ErrorWarning= ValueError("Invalid input, insert correct occupation using non numeric values")
-            print(ErrorWarning)# printing the error warning
-        else: #else if the occupation is correct
-            break
+#     # try catch to read in valid non numeric occupation
+#     while(True):
+#         try:
+#             Occupation= str(input("What is their occupation?"))# prompt the user
+#         except:
+#             raise ValueError('Occupation is not valid')# if invalid say invalid
+#         if Occupation.isnumeric(): #chek if occupation contains only numeric value or not
+#             ErrorWarning= ValueError("Invalid input, insert correct occupation using non numeric values")
+#             print(ErrorWarning)# printing the error warning
+#         else: #else if the occupation is correct
+#             break
     
-    # try catch me to get valid address
-    while(True):
-        try:
-            Address=str(input("What is this persons Address?: "))
-        except:
-            raise ValueError('Address  is not valid')
-        if Address.isnumeric(): #chek if address contains only numeric value or not
-            ErrorWarning= ValueError("address was invalid")
-            print(ErrorWarning)
-        else: #else if the occupation is correct
-            break
+#     # try catch me to get valid address
+#     while(True):
+#         try:
+#             Address=str(input("What is this persons Address?: "))
+#         except:
+#             raise ValueError('Address  is not valid')
+#         if Address.isnumeric(): #chek if address contains only numeric value or not
+#             ErrorWarning= ValueError("address was invalid")
+#             print(ErrorWarning)
+#         else: #else if the occupation is correct
+#             break
 
-    # format the output for each individual person to be this  formatted
-    print(FirstName, LastName, " aged", Age," years, worked as a ",Occupation,
-             " and currently lives at ",Address,".\n")
-    i+=1# increment the ocunter variable go back to loop begining until satisfied
-# end of the program exitting out
+#     # format the output for each individual person to be this  formatted
+#     print(FirstName, LastName, " aged", Age," years, worked as a ",Occupation,
+#              " and currently lives at ",Address,".\n")
+#     i+=1# increment the ocunter variable go back to loop begining until satisfied
+# # end of the program exitting out

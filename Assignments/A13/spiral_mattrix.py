1483403ffd28be4fb09bc9e1a9a86211d64814d9
Loic <konanloic10@gmail.com>
2021-10-25 20:56:09 -0500

    README.md

--- Assignments/A13/spiral_mattrix.py
+++ Assignments/A13/spiral_mattrix.py
@@ -16,26 +16,29 @@ def SpiralMatrix(right, down, array):
     # Perform those for loops.
 	while (left < right and Up < down): 
  
-                                                            # Display the first column from the remaining columns 
+                                                            
 		for i in range(Up, down): 
-			print(array[left][i], end =" ") 
+			print(array[left][i], end =" ")                 # Display the first column from the remaining columns 
 
 		left += 1                                           # Here we increment the left variable by 1.               
 
+                                                            
 		for i in range(left, right): 
-			print(array[i][down - 1], end =" ") 
+			print(array[i][down - 1], end =" ")             # Display the last column from the remaining columns
 
 		down -= 1                                           # Here we decrement the down variable by 1.
 
+        # when the left pointer become 
+        # greater than the right pointer
 		if (left < right): 
 
-			for i in range(down - 1, (Up - 1), -1): 
+			for i in range(down - 1, (Up - 1), -1):         # Display the last row from the remaining rows 
 				print(array[right - 1][i], end =" ") 
 
 			right -= 1                                      # Here we decrement the right variable by 1.
  
 		if (Up < down): 
-			for i in range(right - 1, left - 1, -1): 
+			for i in range(right - 1, left - 1, -1):        # Display the first column from the remaining rows
 				print(array[i][Up], end=" ") 
 
 			Up += 1                                         # Here we increment the Up variable by 1.

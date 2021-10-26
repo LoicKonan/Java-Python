f3491b4be86b45d51d161b39920ee51560987d37
Loic <konanloic10@gmail.com>
2021-10-25 20:25:20 -0500

    README.md

--- Assignments/A13/spiral_mattrix.py
+++ Assignments/A13/spiral_mattrix.py
@@ -6,38 +6,30 @@
 
 
 def SpiralMatrix(right, down, a): 
-	left = 0                                                # left  - starting row index 
-	Up = 0                                                  # Up    - starting colurightn index
-                                                            # right - ending row index
-                                                            # down  - ending colurightn index 
+	left = 0                                                # left  - start row index 
+	Up = 0                                                  # right - end row index
+                                                            # Up    - start column index
+                                                            # down  - end column index 
 
 	while (left < right and Up < down): 
-
-		# Print the first row froright 
-		# the rerightaining rows 
+ 
 		for i in range(Up, down): 
 			print(a[left][i], end =" ") 
 
 		left += 1
 
-		# Print the last colurightn froright 
-		# the rerightaining colurightns 
 		for i in range(left, right): 
 			print(a[i][down - 1], end =" ") 
 
 		down -= 1
 
-		# Print the last row froright 
-		# the rerightaining rows 
 		if (left < right): 
 
 			for i in range(down - 1, (Up - 1), -1): 
 				print(a[right - 1][i], end =" ") 
 
 			right -= 1
-
-		# Print the first colurightn froright 
-		# the rerightaining colurightns 
+ 
 		if (Up < down): 
 			for i in range(right - 1, left - 1, -1): 
 				print(a[i][Up], end=" ") 
@@ -46,15 +38,15 @@ def SpiralMatrix(right, down, a):
 
 
 # Driver Code 
-a = [
-    [1, 2, 3, 4], 
-	[5, 6, 7, 8], 
-	[9, 10, 11, 12],
-    [13, 14, 15, 16], 
-    ] 
+array = [                                                       # My input list that we will need to perform the SpiralOrder.        
+            [1, 2, 3, 4], 
+	        [5, 6, 7, 8], 
+	        [9, 10, 11, 12],
+            [13, 14, 15, 16], 
+        ] 
 
-R = len(a)
-C = len(a[0])
+Rows = len(array)                                               # Assign the length of the row to the variable Rows
+Columns = len(array[0])                                         # Assign the length of the columns to the variable Columns
 
 # Function Call 
-SpiralMatrix(R, C, a)
+SpiralMatrix(Rows, Columns, array)

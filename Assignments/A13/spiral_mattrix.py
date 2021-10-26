ad057f477b1d4442813191cb1cf0768848a15b12
Loic <konanloic10@gmail.com>
2021-10-25 20:28:40 -0500

    README.md

--- Assignments/A13/spiral_mattrix.py
+++ Assignments/A13/spiral_mattrix.py
@@ -5,7 +5,7 @@
 # 
 
 
-def SpiralMatrix(right, down, a): 
+def SpiralMatrix(right, down, array): 
 	left = 0                                                # left  - start row index 
 	Up = 0                                                  # right - end row index
                                                             # Up    - start column index
@@ -14,25 +14,25 @@ def SpiralMatrix(right, down, a):
 	while (left < right and Up < down): 
  
 		for i in range(Up, down): 
-			print(a[left][i], end =" ") 
+			print(array[left][i], end =" ") 
 
 		left += 1
 
 		for i in range(left, right): 
-			print(a[i][down - 1], end =" ") 
+			print(array[i][down - 1], end =" ") 
 
 		down -= 1
 
 		if (left < right): 
 
 			for i in range(down - 1, (Up - 1), -1): 
-				print(a[right - 1][i], end =" ") 
+				print(array[right - 1][i], end =" ") 
 
 			right -= 1
  
 		if (Up < down): 
 			for i in range(right - 1, left - 1, -1): 
-				print(a[i][Up], end=" ") 
+				print(array[i][Up], end=" ") 
 
 			Up += 1
 

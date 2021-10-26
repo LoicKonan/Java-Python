a04f5f5ce4f9ce691244fe43687d7482064898f0
Loic <konanloic10@gmail.com>
2021-10-25 19:57:37 -0500

    README.md

--- Assignments/A13/spiral_mattrix.py
+++ Assignments/A13/spiral_mattrix.py
@@ -1,50 +1,63 @@
-def spiralPrint(m, n, a) : 
-    k = 0; l = 0
-
-    ''' k - starting row index 
-        m - ending row index 
-        l - starting column index 
-        n - ending column index 
-        i - iterator '''
+# Python
+# @author Loic Konan
+# Semester: Fall 2021
+# Date: 10/25/2021 
+# 
 
 
-    while (k < m and l < n) : 
+def SpiralMatrix(m, n, a): 
+	k = 0
+	l = 0
 
-        # Print the first row from 
-        # the remaining rows
-        for i in range(l, n) : 
-            print(a[k][i], end = " ") 
+	''' k - starting row index 
+		m - ending row index 
+		l - starting column index 
+		n - ending column index 
+		i - iterator '''
 
-        k += 1
+	while (k < m and l < n): 
 
-        # Print the last column from 
-        # the remaining columns
-        for i in range(k, m) : 
-            print(a[i][n - 1], end = " ") 
+		# Print the first row from 
+		# the remaining rows 
+		for i in range(l, n): 
+			print(a[k][i], end=" ") 
 
-        n -= 1
+		k += 1
 
-        # Print the last row from 
-        # the remaining rows
-        if ( k < m) : 
+		# Print the last column from 
+		# the remaining columns 
+		for i in range(k, m): 
+			print(a[i][n - 1], end=" ") 
 
-            for i in range(n - 1, (l - 1), -1) : 
-                print(a[m - 1][i], end = " ") 
+		n -= 1
 
-            m -= 1
+		# Print the last row from 
+		# the remaining rows 
+		if (k < m): 
 
-        # Print the first column from 
-        # the remaining columns
-        if (l < n) : 
-            for i in range(m - 1, k - 1, -1) : 
-                print(a[i][l], end = " ") 
+			for i in range(n - 1, (l - 1), -1): 
+				print(a[m - 1][i], end=" ") 
 
-            l += 1
+			m -= 1
+
+		# Print the first column from 
+		# the remaining columns 
+		if (l < n): 
+			for i in range(m - 1, k - 1, -1): 
+				print(a[i][l], end=" ") 
+
+			l += 1
+
 
 # Driver Code 
-a = [ [1, 2, 3, 4, 5, 6], 
-      [7, 8, 9, 10, 11, 12], 
-      [13, 14, 15, 16, 17, 18] ] 
+a = [[1, 2, 3, 4, 5, 6], 
+	[7, 8, 9, 10, 11, 12], 
+	[13, 14, 15, 16, 17, 18],
+    [19,20,21,22,23,24],
+    [25,26,27,28,29,30]] 
 
-R = 3; C = 6
-spiralPrint(R, C, a)
+R = len(a)
+C = len(a[0])
+
+# Function Call 
+SpiralMatrix(R, C, a)

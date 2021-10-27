007e1b8dfb4a1a0fed0314a7f041e5af8cd1863e
Loic <konanloic10@gmail.com>
2021-10-27 14:38:22 -0500

    README.md

--- Assignments/A14/main.py
+++ Assignments/A14/main.py
@@ -1,3 +1,9 @@
+# Python
+# @author Loic Konan
+# Semester: Fall 2021
+# Date: 10/27/2021 
+# 
+
 # abc = '3410 taft blvd, wichita Falls, tx, 76340'
 
 # abc = abc.split(',')
@@ -14,8 +20,31 @@
 
 # print(addcheck)
 
-email = 'saikat.das@msutexas.edu'
-addy = email.split('@')
+# email = 'saikat.das@msutexas.edu'
+# addy = email.split('@')
 
-domain = email[1].split('.')
-print(domain)
+# domain = email[1].split('.')
+# print(domain)
+
+d1 = dict()
+'''
+    'python' : 'snake'
+    'c++'    : 'programing language'
+'''
+
+d1['python'] = 'snake'
+d1['c++'] = 'programing language'
+
+
+d1['anaconda'] = 'big snake'
+d1['c'] = 'basic' 
+
+# can't do enumerate in dictionary.
+for i in d1:   
+    print(i,d1[i])
+    
+if  'python' in d1:
+    val = d1['python']
+    print('found it', val)
+else:
+    print('not found it')

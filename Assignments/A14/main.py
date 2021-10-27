efc7db6b8a58fdd12d14779b9899291cce888ec0
Loic <konanloic10@gmail.com>
2021-10-27 14:22:00 -0500

    README.md

---
+++ Assignments/A14/main.py
@@ -0,0 +1,21 @@
+# abc = '3410 taft blvd, wichita Falls, tx, 76340'
+
+# abc = abc.split(',')
+
+# zipcode = abc[-1]
+
+# state = abc[-2]
+
+# addcheck = abc[-3:]
+
+# city = abc[0].strip()
+# city = abc[1].strip()
+# city = abc[2].strip()
+
+# print(addcheck)
+
+email = 'saikat.das@msutexas.edu'
+addy = email.split('@')
+
+domain = email[1].split('.')
+print(domain)

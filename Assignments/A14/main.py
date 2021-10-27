804a5e92bb6dfdeb123d7c4ac2deffa9c1fc457a
Loic <konanloic10@gmail.com>
2021-10-27 16:06:19 -0500

    README.md

--- Assignments/A14/main.py
+++ Assignments/A14/main.py
@@ -6,75 +6,82 @@
 
 # abc = '3410 taft blvd, wichita Falls, tx, 76340'
 
-# abc = abc.split(',')
+# # abc = abc.split(',')
 
-# zipcode = abc[-1]
+# # zipcode = abc[-1]
 
-# state = abc[-2]
+# # state = abc[-2]
 
-# addcheck = abc[-3:]
+# # addcheck = abc[-3:]
 
-# city = abc[0].strip()
-# city = abc[1].strip()
-# city = abc[2].strip()
+# # city = abc[0].strip()
+# # city = abc[1].strip()
+# # city = abc[2].strip()
 
-# print(addcheck)
+# # print(addcheck)
 
-# email = 'saikat.das@msutexas.edu'
-# addy = email.split('@')
+# # email = 'saikat.das@msutexas.edu'
+# # addy = email.split('@')
 
-# domain = email[1].split('.')
-# print(domain)
+# # domain = email[1].split('.')
+# # print(domain)
 
-d1 = dict()
-'''
-    'python' : 'snake'
-    'c++'    : 'programing language'
-'''
+# d1 = dict()
+# '''
+#     'python' : 'snake'
+#     'c++'    : 'programing language'
+# '''
 
-d1['python'] = 'snake'
-d1['c++'] = 'programing language'
+# d1['python'] = 'snake'
+# d1['c++'] = 'programing language'
 
 
-d1['anaconda'] = 'big snake'
-d1['c'] = 'basic' 
+# d1['anaconda'] = 'big snake'
+# d1['c'] = 'basic' 
 
-# can't do enumerate in dictionary.
-for i in d1:   
-    print(i,d1[i])
+# # can't do enumerate in dictionary.
+# for i in d1:   
+#     print(i,d1[i])
     
-if  'python' in d1:
-    val = d1['python']
-    print('found it', val)
-else:
-    print('not found it')
+# if  'python' in d1:
+#     val = d1['python']
+#     print('found it', val)
+# else:
+#     print('not found it')
 
-val = d1.get('anaconda', 0)     # if key not found, it will return 0 or whatever number.
-print(val)
+# val = d1.get('anaconda', 0)     # if key not found, it will return 0 or whatever number.
+# print(val)
 
-slist = dict()
-slist = {
-    'eggs': 12,
-    'pizza' : 2,
-    'potato' : 2,
-    'anaconda' : 24    
-}
+# slist = dict()
+# slist = {
+#     'eggs': 12,
+#     'pizza' : 2,
+#     'potato' : 2,
+#     'anaconda' : 24    
+# }
 
-val = slist.get("anaconda", 0)
-print(val)
+# val = slist.get("anaconda", 0)
+# print(val)
 
-student = dict()
-stu_list = ['I am happy today hello what up ff']
+# student = dict()
+# stu_list = ['I am happy today hello what up ff']
 
-word = stu_list.split('')
+# word = stu_list.split('')
 
-for i in stu_list:
-    if i in slist:   
-        slist[i] += 1
-    else:   
-        slist[i] = 1
+# for i in stu_list:
+#     if i in slist:   
+#         slist[i] += 1
+#     else:   
+#         slist[i] = 1
         
-print(slist)
+# print(slist)
 
-for i in slist:   
+# for i in slist:   
     
+l1 = [1,3,4,7,8,5,2]
+
+target = 13
+
+def sumoftwo(l1, target):  
+    l2 = [8,5] 
+    return l2    

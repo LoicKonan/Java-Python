70657219761b32ac7dad73d6689bbe0a34170288
Loic <konanloic10@gmail.com>
2021-10-28 18:15:01 -0500

    README.md

--- Assignments/A14/sum_of_two.py
+++ Assignments/A14/sum_of_two.py
@@ -3,14 +3,19 @@
 # Semester: Fall 2021
 # Date: 10/27/2021 
 # 
-class Solution(object):
-    def twoSum(self, nums, target):
-        required = {}
-        for i in range(len(nums)):
-            if target - nums[i] in required:
-                return [required[target - nums[i]],i]
-            else:
-                required[nums[i]]=i
-input_list = [2,8,12,15]
-ob1 = Solution()
-print(ob1.twoSum(input_list, 20))
+
+l1 = [1, 3, 4, 7, 8, 5, 2]
+target = 13
+
+def sumoftwo(l1,target):         
+    d1 = dict()
+
+    for i in l1:
+        remaining = target - i   
+        if remaining in d1:
+            return [remaining - i]
+        else:
+            d1[i] = None
+            
+            
+print(sumoftwo(l1,target))

8e379e88b0d21fc48ab42e6957b36522f316778c
Loic <konanloic10@gmail.com>
2021-10-28 17:56:53 -0500

    README.md

--- Assignments/A14/sum_of_two.py
+++ Assignments/A14/sum_of_two.py
@@ -4,18 +4,34 @@
 # Date: 10/27/2021 
 # 
 
-l1 = [1, 3, 4, 7, 8, 5, 2]
+# l1 = [1, 3, 4, 7, 8, 5, 2]
+# target = 13
+
+# def sumoftwo(l1,target):         
+#     d1 = dict()
+#     for i in l1:
+#         remaining = target - i   
+#         d1[i] = remaining
+#         if remaining in d1:
+#             return [remaining - i]
+#         else:
+#             d1[i] = None
+            
+            
+
+# type nums: List[int]
+# type target: int
+# rtype: List[int]
+
+input_list =  [1, 3, 4, 7, 8, 5, 2]
 target = 13
 
-def sumoftwo(l1,target):         
-    d1 = dict()
-    for i in l1:
-        remaining = target - i   
-        
-        if remaining in d1:
-            return [remaining - i]
+def twoSum(input_list, target):
+    empty_dictionary = ()
+    for i in range(len(nums)):
+        if target - nums[i] in required:
+            return [required[target - nums[i]],i]
         else:
-            d1[i] = None
+            required[nums[i]]=i
             
-            
-print(sumoftwo(l1,target))
+print(twoSum(input_list, target))

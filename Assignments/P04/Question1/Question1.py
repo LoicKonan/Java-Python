086f60fc113239a78cb50f0998c071a306e074db
Loic <konanloic10@gmail.com>
2021-11-02 00:51:13 -0500

    README.md

---
+++ Assignments/P04/Question1/Question1.py
@@ -0,0 +1,20 @@
+ #****************************************************************************
+ # Author:          Loic Konan 
+ # Email:           loickonan.lk@gmail.com 
+ # Label:           Question 1 
+ # Title:           Programing Assignment 04 
+ # Course:          CMPS 4143 
+ # Semester:        Fall 2021 
+ # Date:            11/10/2021
+ # 
+ # Description:     
+ #                  
+ #                  
+ # 
+ # Solution:
+ #                  
+ #
+ #
+ # Files:           Question1.py
+ # 
+ #****************************************************************************/
---
+++ Assignments/P04/Question3/README.md
@@ -0,0 +1,21 @@
+Person’s information
+
+### Loic Konan
+
+#### Description
+
+>
+> **3.**
+>
+> 
+>
+>
+### Files
+
+|   #   | File                         | Description |
+| :---: | ---------------------------- | ----------- |
+|   1   | [Question3.py](Question3.py) | Solution    |
+
+### Instructions
+
+- Complied using **VScode** and **Python 3.9.2**

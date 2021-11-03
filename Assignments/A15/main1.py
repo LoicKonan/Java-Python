8db0f2c2adeb0992df6525601a6cdeb3df14d62b
Loic <konanloic10@gmail.com>
2021-11-03 14:23:08 -0500

    README.md

--- Assignments/A15/main1.py
+++ Assignments/A15/main1.py
@@ -2,9 +2,14 @@ import re
 
 slist = 'here I am frfddd I am frsafse am'
 
-print(str1.startswith('here1'))
+print(slist.startswith('here1'))
 
 
 for s in slist:    
     print(re.seach('^here', s))
     print(s, '\n')
+
+hand = open('sample.txt')   
+for line in hand:    
+    if re.search('^From:', line): 
+        print(line)

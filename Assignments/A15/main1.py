d9e67f88c41af184dc30dbaea620d92ce74b93d8
Loic <konanloic10@gmail.com>
2021-11-03 14:15:20 -0500

    README.md

--- Assignments/A15/main1.py
+++ Assignments/A15/main1.py
@@ -1,9 +1,10 @@
 import re
 
-str1 = 'here I am frfddd I am frsafse am'
+slist = 'here I am frfddd I am frsafse am'
 
 print(str1.startswith('here1'))
 
-subStr = 'I am'
 
-pos = re.search(subStr, str1)
+for s in slist:    
+    print(re.seach('^here', s))
+    print(s, '\n')

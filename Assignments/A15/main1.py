bf107e2bed01bf52c15523ba3821e91f652eb5ab
Loic <konanloic10@gmail.com>
2021-11-03 14:32:53 -0500

    README.md

--- Assignments/A15/main1.py
+++ Assignments/A15/main1.py
@@ -6,10 +6,34 @@ print(slist.startswith('here1'))
 
 
 for s in slist:    
-    print(re.seach('^here', s))
+    # print(re.seach('^here', s))
     print(s, '\n')
 
-hand = open('sample.txt')   
-for line in hand:    
-    if re.search('^From:', line): 
-        print(line)
+# hand = open('sample.txt')   
+# for line in hand:    
+#     if re.search('^From:', line): 
+#         # print(line)
+
+
+l1 = [11,11,11,11,11,11,11,11,11,11,11]
+print(set(l1))        
+                        # for unique thing use set. key values use dictionary. List => stack and queue.
+                        # tuple => strings. for anagram program use the tuples and dictionary. 
+                        # a list of 26 characters  long inialized with 0 
+                        # if you find it add 1 if there is 2 add 2.
+                        #
+
+''' 
+    anagram
+    
+    0 0 2 0 1 0 0 0 0 1 1 0 1 0
+
+    closce
+    lcocse
+
+'''
+
+a = set()
+a.add(l1)
+print(set(a))
+

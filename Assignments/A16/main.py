d77f90912304da37d2b40a3e2a6cf85a85431c65
Loic <konanloic10@gmail.com>
2021-11-08 14:23:39 -0600

    README.md

---
+++ Assignments/A16/main.py
@@ -0,0 +1,31 @@
+class Dummy:
+    # instance variables
+    print("Hello world!")
+    pi = 3.14
+    grade = 0
+    name = ""
+    
+    # Constructor
+    def __init__(self, a=0, b=""):   
+        self.grade = a 
+        self.name = b
+    
+    #instance method
+    def close(self):  
+        print("Close!")
+    
+    #instance methods
+    def greeting(self, g):         # every method in the class need self.
+        print(self.pi)
+        self.close()
+    pass                # if you not doing anything in the class.
+
+
+obj1 = Dummy(23, 'saikat')
+obj1 = Dummy()
+g = "hello world"
+
+obj1.greeting(g)
+obj1.close()
+
+print("Hello world!")

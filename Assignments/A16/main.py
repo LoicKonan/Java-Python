22bee171c00cf3a6cd0e1cae754a55e2394b1bf3
Loic <konanloic10@gmail.com>
2021-11-08 14:40:34 -0600

    README.dm

--- Assignments/A16/main.py
+++ Assignments/A16/main.py
@@ -10,12 +10,23 @@ class Dummy:
         self.grade = a 
         self.name = b
     
+    # mutator and accessor
+    def set_grade(self, newGrade):   
+        self.grade = newGrade
+        
+        
+    def get_grade(self):  
+        return self.grade
+        
     #instance method
     def close(self):  
         print("Close!")
     
     #instance methods
     def greeting(self, g):         # every method in the class need self.
+        self.set_grade(0)
+        
+        print("I decrement your grade to zero")
         print(g, self.name)
         self.close()
     #pass                          # if you not doing anything in the class.
@@ -28,4 +39,17 @@ g = "hello world"
 obj1.greeting(g)
 obj1.close()
 
-print("Hello world!")
+oldGrade = obj1.get_grade()
+obj1.setGrade(oldGrade + 10)
+print("your current grade is: ", obj1.grade)
+
+
+obj2 = Dummy(100, 'LKA')
+
+
+print(obj1)
+print(obj2)
+
+
+class Student(Dummy): 
+    pass 

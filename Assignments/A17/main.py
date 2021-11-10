27bfb70871423646291d9af9c2e8778cf3fed4a4
Loic <konanloic10@gmail.com>
2021-11-10 15:20:50 -0600

    README.md

--- Assignments/A17/main.py
+++ Assignments/A17/main.py
@@ -6,6 +6,31 @@ class Node:
     
 class LinkedList:
     def __init__(self, nodes = None):
+        self.head = None
+        if nodes is not None:
+            node = Node(nodes.pop(0))
+            self.head = node
+            for element in nodes:
+                node.next = Node(element) 
+                node = Node.next
+
+
+def __repr__(self):
+    node = self.head
+    nodes = []
+    
+    while node is not None:
+        nodes.append(node.data)
+        node = Node.next
+    
+    nodes.append("None")
+    return "->".join(nodes)    
+    
+print(LL1)
+            
         
+        
+l1 = ['B', 'C', 'D', 'E']
+LL1 = LinkedList(l1)
         
         

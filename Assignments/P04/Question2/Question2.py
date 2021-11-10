e056947bd889dda74c6c66e8a67130229c72b7a9
Loic <konanloic10@gmail.com>
2021-11-09 23:11:59 -0600

    README.md

--- Assignments/P04/Question2/Question2.py
+++ Assignments/P04/Question2/Question2.py
@@ -27,41 +27,32 @@
  # because it is not sufficiently generic at all. 
  # There are many other characters that exist besides a-z
  
-# define the function definition and it processes
-# will be calle whenever the function is being mention inside of the main
-# portion of our program
-def anagrams(Values):
+def alphabet_map(word):
+        alpha_count_dict = dict(zip('abcdefghijklmnopqrstuvwxyz',[0]*26))
+        for alpha in word:
+            if alpha in alpha_count_dict.keys():
+                alpha_count_dict[alpha] += 1
+            else:
+                alpha_count_dict.update(dict(alpha = 0))
+        return tuple(alpha_count_dict.values())
+
+def anagram_counter(words):
     Dictionary = {}
-    #Loop through all items in the list
-    for i in Values:
-        #Sort the word
-        key = ''.join(sorted(i))
-        #We must check to make sure the word doesnt exist already
+    for word in words:
+        key = alphabet_map(word)
         if key not in Dictionary:
             Dictionary[key] = []
-        #add the word to the group
-        Dictionary[key].append(i)
-    #convert the dictionary values to a list and return them
-    return [Values for key,Values in Dictionary.items()]
+        Dictionary[key].append(word)
+    return [words for key,words in Dictionary.items()]
 
+words = ['eat','tea','tan','ate','nat','bat']
+print (anagram_counter(words))
 
-#------- main portion of our program-----------------
-
-    # read in the first string
-StringList = ['eat','tea','tan','ate','nat','bat']
-print(anagrams(StringList))
 # reading the second sample string
 StringList = ['']
 # go to function definition to determine the output
-print(anagrams(StringList))
+print(anagram_counter(StringList))
 
 # reading the thir string 
 StringList = ['a']
-print(anagrams(StringList))
-
-# now we create a prompt for user to try
-userlist = [] 
-userlist = [item for item in input("Lets test yours : ").split()]
-# see if any matches found
-print(anagrams(userlist))
-
+print(anagram_counter(StringList))

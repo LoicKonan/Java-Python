7c405a2de7342d9cb8151dce0e17c598b7a5c233
Loic <konanloic10@gmail.com>
2021-11-10 16:38:34 -0600

    README.md

---
+++ Assignments/A17/LinkList.py
@@ -0,0 +1,32 @@
+class Node:
+      def init(self, data=None):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+  def init(self, nodes=None):
+    self.head = None
+    if nodes is not None:
+      node = Node(nodes.pop(0))
+      self.head = node
+      for element in nodes:
+        node.next = Node(element)
+        node = node.next
+
+  def repr(self):
+    node = self.head
+    nodes = []
+    while node is not None:
+      nodes.append(node.data)
+      node = node.next
+    nodes.append('None')
+    return "->".join(nodes)
+
+
+l1 = ['3', '2', '1']
+l2 = ['6', '5', '4']
+ll1 = LinkedList(l1)
+ll2 = LinkedList(l2)
+print(ll1)
+print(ll2)

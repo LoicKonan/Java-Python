f3622fc7e39917606be7b287ccdaa4cc8ff41a6d
Loic <konanloic10@gmail.com>
2021-11-15 21:36:53 -0600

    README.md

--- Assignments/A18/MinStack.py
+++ Assignments/A18/MinStack.py
@@ -1,21 +1,30 @@
-# November 15,2021
+# Python
+# @author Loic Konan
+# Semester: Fall 2021
+# Date: 11/15/2021 
 
 class MinStack(): 
+    
+    # Initialized the stack object.
     def __init__(self):
         self.data = [] 
         self.min = None
 
+    # Pushes the element value onto the stack.
     def push(self, item): 
         self.data.append(item)
         self.minimum()
 
+    # Removes the element on the top of the stack.
     def pop(self): 
-        if self.isEmpty():
-            print("The Stack is currently empty")
-        else:
-            print("Pop element was successful")
-            return self.data.pop()
+        t = self.data[-1]
+        self.data.pop()
+        if self.min == t:
+            self.min = self.data[-1]
+            self.data.pop()
 
+    # using this to see if the stack is empty or not and
+    # assign the right value to the min.
     def minimum(self): 
         if self.min is None:
             self.min = self.viewtop()
@@ -23,21 +32,26 @@ class MinStack():
             if self.viewtop() < self.min:
                 self.min = self.viewtop()
 
-    def Min(self): 
+    # Retrieving the minimum element in the stack.
+    def getMin(self): 
         return self.min
 
+    # Retrieves the top element of the stack.
     def viewtop(self): 
         try:
             return self.data[-1]
         except IndexError as e:
             print(e)
 
+    # Return the size of the stock.
     def size(self): 
         return len(self.data)
 
+    # Check if the stack is empty.
     def isEmpty(self): 
         return self.size() == 0
     
+    #return the top of the Minstack.
     def top(self):
         return self.data[-1]
 
@@ -47,8 +61,8 @@ stack = MinStack()
 stack.push(-2)
 stack.push(0)
 stack.push(-3)
-print("Return MinStack : ", stack.Min())
+print("Return MinStack : ", stack.getMin())
 stack.pop()
 print("Return the Top of MinStack : ", stack.top())
-print("Return MinStack : ", stack.Min())
+print("Return MinStack : ", stack.getMin())
 
--- Assignments/A18/README.md
+++ Assignments/A18/README.md
@@ -1,9 +1,23 @@
-## Assignment 18 - 
+## Assignment 18 - Min Stack in Python
 
 ### Loic Konan
 
 ### Description
-
+>
+> - Initialize the stack by min element as infinity
+> - For push operation push(x)
+>       - if x < min, then update min := x
+>       - push x into stack
+> - For pop operation pop()
+>       - t := top element
+>       - delete t from stack
+>       - if t is min, then min := top element of the stack
+> - For top operation top()
+>       - simply return the top element
+>       - for getMin operation getMin()
+>       - return the min element
+>
+>
 ### Files
 
 |   #   | File               | Description |

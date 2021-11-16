772ac67dc35ed447ee69c57ad3110e69191333fd
Loic <konanloic10@gmail.com>
2021-11-16 01:24:38 -0600

    README.md

--- Assignments/P05/Question2/Question2.py
+++ Assignments/P05/Question2/Question2.py
@@ -18,3 +18,69 @@
  # Files:           Question2.py
  # 
  #****************************************************************************/
+
+# Function to find duplicate parenthesis
+# in a balanced expression
+def findDuplicateparenthesis(string):
+
+	# create a stack of characters
+	Stack = []
+
+	# Iterate through the given expression
+	for ch in string:
+	
+		# if current character is
+		# close parenthesis ')'
+		if ch == ')':
+		
+			# pop character from the stack
+			top = Stack.pop()
+
+			# stores the number of characters between
+			# a closing and opening parenthesis
+			# if this count is less than or equal to 1
+			# then the brackets are redundant else not
+			elementsInside = 0
+			while top != '(':
+			
+				elementsInside += 1
+				top = Stack.pop()
+			
+			if elementsInside < 1:
+				return True
+
+		# push open parenthesis '(', operators
+		# and operands to stack
+		else:
+			Stack.append(ch)
+	
+	# No duplicates found
+	return False
+
+# Driver Code
+if __name__ == "__main__":
+  
+    # input balanced expression
+	string = "((x+y))+z"
+
+	if findDuplicateparenthesis(string) == True:
+		print("True")
+	else:
+		print("False")
+  
+    # input balanced expression
+	string1 = "(x+y)"
+
+	if findDuplicateparenthesis(string1) == True:
+		print("True")
+	else:
+		print("False")
+
+
+    # input balanced expression
+	string2 = "((x+y)+((z)))"
+
+	if findDuplicateparenthesis(string2) == True:
+		print("True")
+	else:
+		print("False")

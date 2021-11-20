c740dd46dc82531a53a4610b8ba7f88c827e2d6c
Loic <konanloic10@gmail.com>
2021-11-19 21:10:17 -0600

    README.md

--- Assignments/P05/Question1/Question1.py
+++ Assignments/P05/Question1/Question1.py
@@ -41,59 +41,60 @@ class Node:
 		self.next = None
 	
 class MinStack(Node):
-	
-	# head is default NULL
-	def __init__(self):
-		self.head = None
-		self.minimum = None
-	
-	# Checks if stack is empty
+    
+    min = float('inf')
+    
+    # Initialized the stack object.
+    def __init__(self):
+           self.min = float('inf')
+           self.head = None
+           
+           # Empty list.
+           self.stack = []
+        
+    # Pushes the element value onto the stack.
+    def push(self, value):
+        
+            # if the value that we adding is less than our current
+            # min value, then we add the value to the stack
+            # then assign the value as the new min value.
+            if value <= self.min:
+                self.stack.append(self.min)
+                self.min = value
+                
+            # otherwise just add the value to the stack.
+            self.stack.append(value)
+            
+    # Removes the element on the top of the stack.
+    def pop(self):
+        
+            # Assign the last element to of the stack to a 
+            # temporary variable then recursively call the pop() function itself. 
+            t = self.stack[-1]
+            self.stack.pop()
+            
+            # if the min value is equal to that temporary variable
+            # then set the value to the temporary variable to the min value
+            # and call the pop() function.
+            if self.min == t:
+                self.min = self.stack[-1]
+                self.stack.pop()
+                
+    # Return the last element on the Minstack.   
+    def top(self):
+            return self.stack[-1]
+    
+    # Retrieving the minimum element in the stack.    
+    def getMin(self):
+            return self.min
+
+
+	# Checks if stack is empty 
 	def empty(self):
 		if self.head == None:
 			return True
 		else:
 			return False
-	
-	# Method to add data to the stack
-	# adds to the start of the stack
-	def push(self, data):
-		
-		if self.head == None:
-			self.head = Node(data)
-			
-		else:
-			newnode = Node(data)
-			newnode.next = self.head
-			self.head = newnode
-	
-	# Remove element that is the current head (start of the stack)
-	def pop(self):
-		
-		if self.empty():
-			return None
-			
-		else:
-			# Removes the head node and makes
-			#the preceding one the new head
-			poppednode = self.head
-			self.head = self.head.next
-			poppednode.next = None
-			return poppednode.data
-	
-	# Returns the head node data
-	def top(self):
-		
-		if self.empty():
-			return None
-			
-		else:
-			return self.head.data
-
-
-	def getMin(self):
-		return self.minimum
-
-
 
 	# Prints out the stack	
 	def display(self):

b87349a3d90478000c10abaef4a7a77cc39d8757
Loic <konanloic10@gmail.com>
2021-11-22 15:59:05 -0600

    README.md

---
+++ Assignments/A20/BDF.py
@@ -0,0 +1,53 @@
+from collections import defaultdict
+
+class Graph:
+  def __init__(self):
+    self.graph = defaultdict(list)
+    self.nodes = 12
+    # function to add an edge to graph
+  def addEdge(self,u,v):
+    self.graph[u].append(v)
+    
+  def BFS(self, root):
+    visited = [False] * (self.nodes+1)
+    # print(len(visited))
+    q = []
+    q.append(root)
+    visited[root] = True
+
+    while len(q)!=0:
+      root = q.pop(0)
+      print(root, end=" ")
+
+      for i in self.graph[root]:
+        if visited[i] == False:
+          q.append(i)
+          visited[i] = True
+
+  def dfs(self, visited, node):
+    if node not in visited:
+      print(node, end=" ")
+      visited.add(node)
+      for neighbor in self.graph[node]:
+        self.dfs(visited, neighbor)
+
+  def printGraph(self):
+    print(self.graph)
+
+g = Graph()
+g.addEdge(1, 2)
+g.addEdge(1, 3)
+
+g.addEdge(2, 4)
+g.addEdge(2, 5)
+
+g.addEdge(3, 6)
+g.addEdge(3, 7)
+
+g.addEdge(4, 8)
+g.addEdge(4, 9)
+
+g.addEdge(5, 10)
+g.addEdge(6, 11)
+g.addEdge(6, 12)
+

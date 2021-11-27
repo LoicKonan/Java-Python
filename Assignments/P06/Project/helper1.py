046cced74635c5411b386599113cda25d254bfbc
Loic <konanloic10@gmail.com>
2021-11-26 23:28:48 -0600

    README.md

--- Assignments/P06/Project/helper1.py
+++ Assignments/P06/Project/helper1.py
@@ -1,86 +1,180 @@
-import sys
-import os
-import pprint
+# import tkinter module
+from tkinter import *
 
-ALPHABET = [chr(x+97) for x in range(26)]
+# import other necessary modules
+import random
+import time
+import datetime
 
-def vigenere_cipher_encrypt(**kwargs):
-    input_file = kwargs.get('input',None)
-    output_file = kwargs.get('output',None)
-    key = kwargs.get('key',None)
+# creating root object
+root = Tk()
 
-    # should test if file exists
-    with open(input_file) as f:
-        plaintext = f.read()
+# defining size of window
+root.geometry("1200x6000")
 
-    plaintext = plaintext.lower()
-    ciphertext = ''
+# setting up the title of window
+root.title("Message Encryption and Decryption")
 
-    i = 0
-    for letter in plaintext:
-        if letter in ALPHABET:
+Tops = Frame(root, width = 1600, relief = SUNKEN)
+Tops.pack(side = TOP)
 
-            a = ord(letter)-97
-            b = ord(key[i])-97
-            ciphertext += chr(((a+b)%26)+97)
+f1 = Frame(root, width = 800, height = 700,
+							relief = SUNKEN)
+f1.pack(side = LEFT)
 
-            i = (i + 1) % len(key)
-        else:
-            ciphertext += letter
+# ==============================================
+#				 TIME
+# ==============================================
+localtime = time.asctime(time.localtime(time.time()))
 
-    with open(output_file,'w') as f:
-        f.write(ciphertext)
+lblInfo = Label(Tops, font = ('helvetica', 50, 'bold'),
+		text = "SECRET MESSAGING \n Vigenère cipher",
+					fg = "Black", bd = 10, anchor='w')
+					
+lblInfo.grid(row = 0, column = 0)
 
-def vigenere_cipher_decrypt(**kwargs):
-    input_file = kwargs.get('input',None)
-    output_file = kwargs.get('output',None)
-    key = kwargs.get('key',None)
+lblInfo = Label(Tops, font=('arial', 20, 'bold'),
+			text = localtime, fg = "Steel Blue",
+						bd = 10, anchor = 'w')
+						
+lblInfo.grid(row = 1, column = 0)
+
+rand = StringVar()
+Msg = StringVar()
+key = StringVar()
+mode = StringVar()
+Result = StringVar()
+
+# exit function
+def qExit():
+	root.destroy()
+
+# Function to reset the window
+def Reset():
+	rand.set("")
+	Msg.set("")
+	key.set("")
+	mode.set("")
+	Result.set("")
 
 
-    # should test if file exists
-    with open(input_file) as f:
-        ciphertext = f.read()
+# reference
+lblReference = Label(f1, font = ('arial', 16, 'bold'),
+				text = "Name:", bd = 16, anchor = "w")
+				
+lblReference.grid(row = 0, column = 0)
 
-    decrypted = ''
-    i = 0
-    for letter in ciphertext:
-        if letter in ALPHABET:
-            a = ord(letter)-97
-            b = ord(key[i])-97
-            decrypted += chr(((a-b)%26)+97)
+txtReference = Entry(f1, font = ('arial', 16, 'bold'),
+			textvariable = rand, bd = 10, insertwidth = 4,
+						bg = "powder blue", justify = 'right')
+						
+txtReference.grid(row = 0, column = 1)
 
-            i = (i + 1) % len(key)
-        else:
-            decrypted += letter
+# labels
+lblMsg = Label(f1, font = ('arial', 16, 'bold'),
+		text = "MESSAGE", bd = 16, anchor = "w")
+		
+lblMsg.grid(row = 1, column = 0)
 
-    with open(output_file,'w') as f:
-        f.write(decrypted)
+txtMsg = Entry(f1, font = ('arial', 16, 'bold'),
+		textvariable = Msg, bd = 10, insertwidth = 4,
+				bg = "powder blue", justify = 'right')
+				
+txtMsg.grid(row = 1, column = 1)
 
-if __name__=='__main__':
-    """
-    Change the required params value below accordingly.
-    """
+lblkey = Label(f1, font = ('arial', 16, 'bold'),
+			text = "KEY", bd = 16, anchor = "w")
+			
+lblkey.grid(row = 2, column = 0)
 
-    required_params = 4 # adjust accordingly
+txtkey = Entry(f1, font = ('arial', 16, 'bold'),
+		textvariable = key, bd = 10, insertwidth = 4,
+				bg = "powder blue", justify = 'right')
+				
+txtkey.grid(row = 2, column = 1)
 
-    # get processed command line arguments 
-    _,params = cmd_params(sys.argv[1:])
+lblmode = Label(f1, font = ('arial', 16, 'bold'),
+		text = "MODE(e for encrypt, d for decrypt)",
+								bd = 16, anchor = "w")
+								
+lblmode.grid(row = 3, column = 0)
 
-    # print usage if not called correctly
-    if len(params) < required_params:
-        usage()
+txtmode = Entry(f1, font = ('arial', 16, 'bold'),
+		textvariable = mode, bd = 10, insertwidth = 4,
+				bg = "powder blue", justify = 'right')
+				
+txtmode.grid(row = 3, column = 1)
 
-    operation = params.get('op',None)
-    infile = params.get('input',None)
-    outfile = params.get('output',None)
-    key = params.get('key',None)
+lblService = Label(f1, font = ('arial', 16, 'bold'),
+			text = "The Result-", bd = 16, anchor = "w")
+			
+lblService.grid(row = 2, column = 2)
 
-    if not operation and not infile and not outfile and not key:
-        usage()
+txtService = Entry(f1, font = ('arial', 16, 'bold'),
+			textvariable = Result, bd = 10, insertwidth = 4,
+					bg = "powder blue", justify = 'right')
+						
+txtService.grid(row = 2, column = 3)
 
-    if operation.lower() == 'encrypt':
-        vigenere_cipher_encrypt(**params)
-    elif operation.lower() == 'decrypt':
-        vigenere_cipher_decrypt(**params)
-    else:
-        usage()
+# Vigenère cipher
+import base64
+
+# Function to encode
+def encode(key, clear):
+	enc = []
+	
+	for i in range(len(clear)):
+		key_c = key[i % len(key)]
+		enc_c = chr((ord(clear[i]) +
+					ord(key_c)) % 256)
+					
+		enc.append(enc_c)
+		
+	return base64.urlsafe_b64encode("".join(enc).encode()).decode()
+
+# Function to decode
+def decode(key, enc):
+	dec = []
+	
+	enc = base64.urlsafe_b64decode(enc).decode()
+	for i in range(len(enc)):
+		key_c = key[i % len(key)]
+		dec_c = chr((256 + ord(enc[i]) -
+						ord(key_c)) % 256)
+							
+		dec.append(dec_c)
+	return "".join(dec)
+
+
+def Ref():
+	print("Message= ", (Msg.get()))
+
+	clear = Msg.get()
+	k = key.get()
+	m = mode.get()
+
+	if (m == 'e'):
+		Result.set(encode(k, clear))
+	else:
+		Result.set(decode(k, clear))
+
+# Show message button
+btnTotal = Button(f1, padx = 16, pady = 8, bd = 16, fg = "black",
+						font = ('arial', 16, 'bold'), width = 10,
+					text = "Show Message", bg = "powder blue",
+						command = Ref).grid(row = 7, column = 1)
+
+# Reset button
+btnReset = Button(f1, padx = 16, pady = 8, bd = 16,
+				fg = "black", font = ('arial', 16, 'bold'),
+					width = 10, text = "Reset", bg = "green",
+				command = Reset).grid(row = 7, column = 2)
+
+# Exit button
+btnExit = Button(f1, padx = 16, pady = 8, bd = 16,
+				fg = "black", font = ('arial', 16, 'bold'),
+					width = 10, text = "Exit", bg = "red",
+				command = qExit).grid(row = 7, column = 3)
+
+# keeps window alive
+root.mainloop()

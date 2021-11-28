8652c2ec281f0364281f197e4fde98b21f6f1992
Loic <konanloic10@gmail.com>
2021-11-27 21:26:07 -0600

    README.md

--- Assignments/P06/Project/Vigenere.py
+++ Assignments/P06/Project/Vigenere.py
@@ -2,7 +2,8 @@ import sys
 import copy
 import time
 
-import tkinter as tk
+
+from tkinter import *
 
 FONT = ("calbri", 20, "bold")
 
@@ -43,33 +44,15 @@ FONT = ("calbri", 20, "bold")
 
 class Vigenere:
 
-    def __init__(self, master):
+    def __init__(self, input_file = None, output_file = None):
 
-        master.title("Byron Dowling & Loic Konan \n Vigenère Cipher")
-        self.plaintext = tk.StringVar(master, value="")
-        self.ciphertext = tk.StringVar(master, value="")
-        self.key = tk.IntVar(master)
+        if not input_file is None:
 
+            self.Input = input_file
 
-        # Plaintext controls
-        self.plain_label = tk.Label(master, text="Plaintext", fg="green", font=FONT).grid(row=0, column=0)
-        self.plain_entry = tk.Entry(master,
-                                    textvariable=self.plaintext, width=50, font=FONT)
-        self.plain_entry.grid(row=0, column=1, padx=20)
-        self.encrypt_button = tk.Button(master, text="Encrypt",
-                                        command=lambda: self.encrypt_callback(), font=FONT).grid(row=0, column=2)
-        self.plain_clear = tk.Button(master, text="Clear",
-                                     command=lambda: self.clear('plain'), font=FONT).grid(row=0, column=3)
+        if not output_file is None:
 
-        # Ciphertext controls
-        self.cipher_label = tk.Label(master, text="Ciphertext", fg="red", font=FONT).grid(row=2, column=0)
-        self.cipher_entry = tk.Entry(master,
-                                     textvariable=self.ciphertext, width=50, font=FONT)
-        self.cipher_entry.grid(row=2, column=1, padx=20)
-        self.decrypt_button = tk.Button(master, text="Decrypt",
-                                        command=lambda: self.decrypt_callback(), font=FONT).grid(row=2, column=2)
-        self.cipher_clear = tk.Button(master, text="Clear",
-                                      command=lambda: self.clear('cipher'), font=FONT).grid(row=2, column=3)
+            self.output = output_file
 
         ## Variables used to keep track of the before and after message/s
         self.Plaintext = None
@@ -87,29 +70,10 @@ class Vigenere:
         self.highest = None
 
         ## List to keep track of the different keys to test
-        self.KeyInfo = []
+        self.KeyInfo =  []
         self.KeyOrder = []
 
-
-    def clear(self, str_val):
-        if str_val == 'cipher':
-            self.cipher_entry.delete(0, 'end')
-        elif str_val == 'plain':
-            self.plain_entry.delete(0, 'end')
-
-
-
-    def encrypt_callback(self, **params):
-        ciphertext = self.Encrypt_Message(**params)
-        self.cipher_entry.delete(0, tk.END)
-        self.cipher_entry.insert(0, ciphertext)
-
-    def decrypt_callback(self, **params):
-        plaintext = self.Index_Of_Coincidence(**params)
-        self.plain_entry.delete(0, tk.END)
-        self.plain_entry.insert(0, plaintext)
-
-
+    
     """
     $$$$$$\ $$\   $$\         $$\   $$\          $$$$$$\  $$\   $$\ $$$$$$$$\ 
     \_$$  _|$$$\  $$ |        $$$\  $$ |        $$  __$$\ $$ |  $$ |\__$$  __|
@@ -365,6 +329,7 @@ class Vigenere:
 
                 index += 1
 
+
             for num in frequency:
 
                 Sigma_Big_N = len(temp)
@@ -586,6 +551,7 @@ class Vigenere:
                 self.EncryptionKey = potkey
 
 
+
 """
 
     $$\                                                             
@@ -654,15 +620,15 @@ if __name__=='__main__':
     """
     Main block
     """
-    
-    root = tk.Tk()    
-    
-    V1 = Vigenere(root)
+    V1 = Vigenere()
 
     argv = sys.argv[1:]                     # strip file name (main.py) out of args
 
     args,program_params = mykwargs(argv)
 
+   
+   
+    
     
     """
             This Try and Except block is mainly used so that the user
@@ -685,7 +651,6 @@ if __name__=='__main__':
         toc = time.perf_counter()
 
         print(f"Operation Completed in {toc-tic:0.4f} Seconds")
-        root.mainloop()
 
 
     except:

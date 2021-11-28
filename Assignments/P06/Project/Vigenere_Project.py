4ec4f8186ba8981ecad093f8cb3570925a4e3860
Loic <konanloic10@gmail.com>
2021-11-28 17:07:56 -0600

    README.md

--- Assignments/P06/Project/Vigenere_Project.py
+++ Assignments/P06/Project/Vigenere_Project.py
@@ -34,7 +34,6 @@ import time
                     $$\   $$ |                                                  
                     \$$$$$$  |                                                  
                      \______/                    
-
 """
 
 class Vigenere:

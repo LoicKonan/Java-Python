468fce4036dd358c22355a4a439501ecb2aecc8e
Loic <konanloic10@gmail.com>
2021-11-27 20:27:25 -0600

    README.md

--- Assignments/P06/Project/Vigenere.py
+++ Assignments/P06/Project/Vigenere.py
@@ -2,162 +2,9 @@ import sys
 import copy
 import time
 
-# import tkinter module
-from tkinter import *
+import tkinter as tk
 
-
-# import necessary modules
-import datetime
-
-# creating root object
-root = Tk()
-
-# defining size of window
-root.geometry("1200x6000")
-
-# setting up the title of window
-root.title("Message Encryption and Decryption")
-
-Tops = Frame(root, width = 1600, relief = SUNKEN)
-Tops.pack(side = TOP)
-
-f1 = Frame(root, width = 800, height = 700,
-							relief = SUNKEN)
-f1.pack(side = LEFT)
-
-# ==============================================
-#				 TIME
-# ==============================================
-localtime = time.asctime(time.localtime(time.time()))
-
-lblInfo = Label(Tops, font = ('helvetica', 50, 'bold'),
-		text = "Byron Dowling & Loic Konan \n Vigenère Cipher",
-					fg = "Black", bd = 10, anchor='w')
-					
-lblInfo.grid(row = 0, column = 0)
-
-lblInfo = Label(Tops, font=('arial', 20, 'bold'),
-			text = localtime, fg = "Steel Blue",
-						bd = 10, anchor = 'w')
-						
-lblInfo.grid(row = 1, column = 0)
-
-Msg = StringVar()
-key = StringVar()
-mode = StringVar()
-Result = StringVar()
-
-# exit function
-def qExit():
-	root.destroy()
-
-# Function to reset the window
-def Reset():
-	Msg.set("")
-	mode.set("")
-	Result.set("")
-
-
-# labels
-lblMsg = Label(f1, font = ('arial', 16, 'bold'),
-		text = "MESSAGE", bd = 16, anchor = "w")
-		
-lblMsg.grid(row = 1, column = 0)
-
-txtMsg = Entry(f1, font = ('arial', 16, 'bold'),
-		textvariable = Msg, bd = 10, insertwidth = 20,
-				bg = "powder blue", justify = 'right')
-				
-txtMsg.grid(row = 1, column = 1)
-
-lblmode = Label(f1, font = ('arial', 16, 'bold'),
-		text = "MODE(e for encrypt, d for decrypt)",
-								bd = 16, anchor = "w")
-								
-lblmode.grid(row = 3, column = 0)
-
-txtmode = Entry(f1, font = ('arial', 16, 'bold'),
-		textvariable = mode, bd = 10, insertwidth = 4,
-				bg = "powder blue", justify = 'right')
-				
-txtmode.grid(row = 3, column = 1)
-
-lblService = Label(f1, font = ('arial', 16, 'bold'),
-			text = "The Result", bd = 16, anchor = "w")
-			
-lblService.grid(row = 2, column = 2)
-
-txtService = Entry(f1, font = ('arial', 16, 'bold'),
-			textvariable = Result, bd = 10, insertwidth = 4,
-					bg = "powder blue", justify = 'right')
-						
-txtService.grid(row = 2, column = 3)
-
-
-# Vigenère cipher
-import base64
-# Function to encode
-def encode(key, clear):
-	enc = []
-	
-	for i in range(len(clear)):
-		key_c = key[i % len(key)]
-		enc_c = chr((ord(clear[i]) +
-					ord(key_c)) % 256)
-					
-		enc.append(enc_c)
-		
-	return base64.urlsafe_b64encode("".join(enc).encode()).decode()
-
-# Function to decode
-def decode(key, enc):
-	dec = []
-	
-	enc = base64.urlsafe_b64decode(enc).decode()
-	for i in range(len(enc)):
-		key_c = key[i % len(key)]
-		dec_c = chr((256 + ord(enc[i]) -
-						ord(key_c)) % 256)
-							
-		dec.append(dec_c)
-	return "".join(dec)
-
-
-def Ref():
-	print("Message= ", (Msg.get()))
-
-
-	clear = Msg.get()
-	k = key.get()
-	m = mode.get()
-
-	if (m == 'e'):
-		Result.set(encode(k, clear))
-	else:
-		Result.set(decode(k, clear))
-
-
-
-# Show message button
-btnTotal = Button(f1, padx = 16, pady = 8, bd = 16, fg = "black",
-						font = ('arial', 16, 'bold'), width = 10,
-					text = "Show Message", bg = "powder blue",
-						command = Ref).grid(row = 7, column = 1)
-
-# Reset button
-btnReset = Button(f1, padx = 16, pady = 8, bd = 16,
-				fg = "black", font = ('arial', 16, 'bold'),
-					width = 10, text = "Reset", bg = "green",
-				command = Reset).grid(row = 7, column = 2)
-
-# Exit button
-btnExit = Button(f1, padx = 16, pady = 8, bd = 16,
-				fg = "black", font = ('arial', 16, 'bold'),
-					width = 10, text = "Exit", bg = "red",
-				command = qExit).grid(row = 7, column = 3)
-
-# keeps window alive
-root.mainloop()
+FONT = ("calbri", 20, "bold")
 
 
 
@@ -198,15 +45,38 @@ root.mainloop()
 
 class Vigenere:
 
-    def __init__(self, input_file = None, output_file = None):
+    def __init__(self, master):
 
-        if not input_file is None:
+        master.title("Byron Dowling & Loic Konan \n Vigenère Cipher")
+        self.plaintext = tk.StringVar(master, value="")
+        self.ciphertext = tk.StringVar(master, value="")
+        self.key = tk.IntVar(master)
 
-            self.Input = input_file
 
-        if not output_file is None:
+        # Plaintext controls
+        self.plain_label = tk.Label(master, text="Plaintext", fg="green", font=FONT).grid(row=0, column=0)
+        self.plain_entry = tk.Entry(master,
+                                    textvariable=self.plaintext, width=50, font=FONT)
+        self.plain_entry.grid(row=0, column=1, padx=20)
+        self.encrypt_button = tk.Button(master, text="Encrypt",
+                                        command=lambda: self.encrypt_callback(), font=FONT).grid(row=0, column=2)
+        self.plain_clear = tk.Button(master, text="Clear",
+                                     command=lambda: self.clear('plain'), font=FONT).grid(row=0, column=3)
 
-            self.output = output_file
+        # # Key controls
+        # self.key_label = tk.Label(master, text="Key", font=FONT).grid(row=1, column=0)
+        # self.key_entry = tk.Entry(master, textvariable=self.key, width=10, font=FONT).grid(row=1, column=1,
+        #                                                                                    sticky=tk.W, padx=20)
+
+        # Ciphertext controls
+        self.cipher_label = tk.Label(master, text="Ciphertext", fg="red", font=FONT).grid(row=2, column=0)
+        self.cipher_entry = tk.Entry(master,
+                                     textvariable=self.ciphertext, width=50, font=FONT)
+        self.cipher_entry.grid(row=2, column=1, padx=20)
+        self.decrypt_button = tk.Button(master, text="Decrypt",
+                                        command=lambda: self.decrypt_callback(), font=FONT).grid(row=2, column=2)
+        self.cipher_clear = tk.Button(master, text="Clear",
+                                      command=lambda: self.clear('cipher'), font=FONT).grid(row=2, column=3)
 
         ## Variables used to keep track of the before and after message/s
         self.Plaintext = None
@@ -224,10 +94,38 @@ class Vigenere:
         self.highest = None
 
         ## List to keep track of the different keys to test
-        self.KeyInfo =  []
+        self.KeyInfo = []
         self.KeyOrder = []
 
+
+    def clear(self, str_val):
+        if str_val == 'cipher':
+            self.cipher_entry.delete(0, 'end')
+        elif str_val == 'plain':
+            self.plain_entry.delete(0, 'end')
+
+
+    # def get_key(self):
+    #     try:
+    #         key_val = self.key.get()
+    #         return key_val
+    #     except tk.TclError:
+    #         pass
     
+
+    def encrypt_callback(self):
+        key = self.get_key()
+        ciphertext = encrypt(self.plain_entry.get(), key)
+        self.cipher_entry.delete(0, tk.END)
+        self.cipher_entry.insert(0, ciphertext)
+
+    def decrypt_callback(self):
+        key = self.get_key()
+        plaintext = decrypt(self.cipher_entry.get(), key)
+        self.plain_entry.delete(0, tk.END)
+        self.plain_entry.insert(0, plaintext)
+
+
     """
     $$$$$$\ $$\   $$\         $$\   $$\          $$$$$$\  $$\   $$\ $$$$$$$$\ 
     \_$$  _|$$$\  $$ |        $$$\  $$ |        $$  __$$\ $$ |  $$ |\__$$  __|
@@ -705,7 +603,6 @@ class Vigenere:
                 self.EncryptionKey = potkey
 
 
-
 """
 
     $$\                                                             
@@ -774,15 +671,15 @@ if __name__=='__main__':
     """
     Main block
     """
-    V1 = Vigenere()
+    
+    root = tk.Tk()    
+    
+    V1 = Vigenere(root)
 
     argv = sys.argv[1:]                     # strip file name (main.py) out of args
 
     args,program_params = mykwargs(argv)
 
-   
-   
-    
     
     """
             This Try and Except block is mainly used so that the user
@@ -805,6 +702,7 @@ if __name__=='__main__':
         toc = time.perf_counter()
 
         print(f"Operation Completed in {toc-tic:0.4f} Seconds")
+        root.mainloop()
 
 
     except:

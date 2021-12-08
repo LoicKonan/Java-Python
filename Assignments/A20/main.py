3825243694c44ec936ba341d9aac82855493d745
Loic <konanloic10@gmail.com>
2021-12-08 04:38:30 -0600

    README.md

--- Assignments/A20/main.py
+++ Assignments/A20/main.py
@@ -15,15 +15,15 @@ class Graph:
     self.graph[u].append(v)
     
   def BFS(self, root):
-    visited = [False] * (self.nodes+1)
+    visited = [False] * (self.nodes + 1)
     # print(len(visited))
     q = []
     q.append(root)
     visited[root] = True
 
-    while len(q)!=0:
+    while len(q) != 0:
       root = q.pop(0)
-      print(root, end=" ")
+      print(root, end = " ")
 
       for i in self.graph[root]:
         if visited[i] == False:
@@ -33,7 +33,7 @@ class Graph:
 
   def dfs(self, visited, node):
     if node not in visited:
-      print(node, end=" ")
+      print(node, end = " ")
       visited.add(node)
       for neighbor in self.graph[node]:
         self.dfs(visited, neighbor)
@@ -42,21 +42,13 @@ class Graph:
     print(self.graph)
 
 g = Graph()
+g.addEdge(0, 1)
+g.addEdge(0, 2)
 g.addEdge(1, 2)
-g.addEdge(1, 3)
+g.addEdge(2, 0)
+g.addEdge(2, 3)
+g.addEdge(3, 3)
 
-g.addEdge(2, 4)
-g.addEdge(2, 5)
-
-g.addEdge(3, 6)
-g.addEdge(3, 7)
-
-g.addEdge(4, 8)
-g.addEdge(4, 9)
-
-g.addEdge(5, 10)
-g.addEdge(6, 11)
-g.addEdge(6, 12)
 
 g.BFS(2)
 
